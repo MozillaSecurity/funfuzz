1960af3b30c8a26d6302c1cc178d27e589549e17
Jesse Ruderman <jruderman@gmail.com>
2008-05-22 16:54:51 -0700

    Make it possible to run DOM fuzzers with two profiles at the same time.

--- dom/automation/af_timed_run.py
+++ dom/automation/af_timed_run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python -u
 
-import sys, random, time
+import sys, random, time, os
 import detect_assertions, detect_leaks, detect_malloc_errors
 sys.path.append("../../lithium/")
 import ntr
@@ -12,7 +12,7 @@ def many_timed_runs(fullURLs):
     for iteration in range(0, len(fullURLs)):
         fullURL = fullURLs[iteration]
         # print "URL: " + URL
-        logPrefix = "w%d" % iteration
+        logPrefix = tempDir + os.sep + "w" + str(iteration)
         (sta, msg, elapsedtime) = ntr.timed_run(sys.argv[3:] + [fullURL], int(sys.argv[1]), logPrefix)
         
         print "%s: %s (%.1f seconds)" % (logPrefix, msg, elapsedtime)
@@ -42,22 +42,37 @@ def getURLs():
         if (not line.startswith("#") and len(line) > 2):
             URLs.append(line.rstrip())
             
-    plan = file("wplan", 'w')
+    plan = file(tempDir + os.sep + "wplan", 'w')
 
     for iteration in range(0, 10000):
         metaSeed = random.randint(1, 10000)
         metaPer = random.randint(0, 15) * random.randint(0, 15) + 5
         u = random.choice(URLs) + "#squarefree-af!fuzzer-combined.js!" + str(metaSeed) + ",0," + str(metaPer) + ",10,3000,0"
         fullURLs.append(u)
-        plan.write("w" + str(iteration) + " = " + u + "\n")
+        plan.write(tempDir + os.sep + "w" + str(iteration) + " = " + u + "\n")
     
     plan.close()
     
     return fullURLs
 
 
+def createTempDir():
+    global tempDir
+    i = 1
+    while 1:
+        tempDir = "wtmp" + str(i)
+        if not os.path.exists(tempDir):
+            os.mkdir(tempDir)
+            break
+        i += 1
+
+
+
+
 if len(sys.argv) >= 4:
+    createTempDir()
     many_timed_runs(getURLs())
 else:
     print "Not enough command-line arguments"
     print "Usage: ./af_timed_run.py timeout urllist firefoxpath [firefoxargs ...]"
+
