bb826c1303f76455b7bbd73b49d418a4da688964
Jesse Ruderman <jruderman@gmail.com>
2008-11-03 21:19:23 -0800

    Retrieve delicious random URLs using Python instead of Firefox, so the URLs we get from delicious get recorded for sure.  Hope this doesn't break too much stuff

--- dom/automation/af_timed_run.py
+++ dom/automation/af_timed_run.py
@@ -4,15 +4,29 @@ import sys, random, time, os
 import detect_assertions, detect_leaks, detect_malloc_errors, detect_interesting_crashes
 sys.path.append("../../lithium/")
 import ntr
+import randomURL
 
+
+urlListFilename = sys.argv[2]
+maxIterations = 100000
+yummy = (urlListFilename == "urls-random")
 
 
 def many_timed_runs(fullURLs):
     
-    for iteration in range(0, len(fullURLs)):
-        fullURL = fullURLs[iteration]
-        # print "URL: " + URL
+    for iteration in range(0, maxIterations):
+
+        if yummy:
+            fullURL = randomURL.randomURL() + randomHash()
+        else:
+            fullURL = fullURLs[iteration]
+
         logPrefix = tempDir + os.sep + "w" + str(iteration)
+
+        c = file(logPrefix + "-url", "w")
+        c.write(fullURL)
+        c.close()
+
         (sta, msg, elapsedtime) = ntr.timed_run(sys.argv[3:] + [fullURL], int(sys.argv[1]), logPrefix)
         
         amissAssert = detect_assertions.amiss(logPrefix, False)
@@ -35,23 +49,22 @@ def many_timed_runs(fullURLs):
         if sta == ntr.NORMAL and not amiss:
             os.remove(logPrefix + "-out")
             os.remove(logPrefix + "-err")
-             
+            os.remove(logPrefix + "-url")
+
 
 def getURLs():
     URLs = []
     fullURLs = []
     
-    urlfile = open(sys.argv[2], "r")
+    urlfile = open(urlListFilename, "r")
     for line in urlfile:
         if (not line.startswith("#") and len(line) > 2):
             URLs.append(line.rstrip())
             
     plan = file(tempDir + os.sep + "wplan", 'w')
 
-    for iteration in range(0, 100000):
-        metaSeed = random.randint(1, 10000)
-        metaPer = random.randint(0, 15) * random.randint(0, 15) + 5
-        u = random.choice(URLs) + "#squarefree-af!fuzzer-combined.js!" + str(metaSeed) + ",0," + str(metaPer) + ",10,3000,0"
+    for iteration in range(0, maxIterations):
+        u = random.choice(URLs) + randomHash()
         fullURLs.append(u)
         plan.write(tempDir + os.sep + "w" + str(iteration) + " = " + u + "\n")
     
@@ -70,13 +83,17 @@ def createTempDir():
             break
         i += 1
 
-
-
+def randomHash():
+    metaSeed = random.randint(1, 10000)
+    metaPer = random.randint(0, 15) * random.randint(0, 15) + 5
+    return "#squarefree-af!fuzzer-combined.js!" + str(metaSeed) + ",0," + str(metaPer) + ",10,3000,0"
 
 if len(sys.argv) >= 4:
     createTempDir()
-    many_timed_runs(getURLs())
+    if yummy: # hacky
+        many_timed_runs(None)
+    else:
+        many_timed_runs(getURLs())
 else:
     print "Not enough command-line arguments"
     print "Usage: ./af_timed_run.py timeout urllist firefoxpath [firefoxargs ...]"
-
