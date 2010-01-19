22357aab4a97f421964f891e7c220f7467959068
Jesse Ruderman <jruderman@gmail.com>
2010-01-18 20:36:48 -0800

    Do all our valgrind-error-ignoring through the valgrind suppressions file, not in python

--- dom/automation/detect_valgrind_errors.py
+++ dom/automation/detect_valgrind_errors.py
@@ -3,41 +3,6 @@
 import sys
 import xml.dom.minidom
 
-(BLAME_MOZILLA, BLAME_DONT_CARE, BLAME_MAC_LIBRARIES, BLAME_UNKNOWN) = range(4)
-
-
-def blame(error):
-    stack = error.getElementsByTagName("stack")[0]
-    for frame in stack.childNodes:
-        if frame.nodeType == frame.ELEMENT_NODE:
-            fns = frame.getElementsByTagName("fn")
-            if len(fns) > 0:
-                fn = fns[0].firstChild.data
-                if fn == "__static_initialization_and_destruction_0(int, int)":
-                    return BLAME_DONT_CARE # XXX TEMPORARY (should try firefox debug or objdump/asm)
-            objs = frame.getElementsByTagName("obj")
-            if len(objs) > 0:
-                # XXX I really should use 'obj:' suppressions rather than this kind of thing.
-                # See http://valgrind.org/docs/manual/manual-core.html#manual-core.suppress
-                obj = objs[0].firstChild.data
-                if obj.find("valgrind") != -1:
-                    pass
-                elif obj.find("nssutil") != -1:
-                    return BLAME_DONT_CARE # NSS
-                elif obj.find("Darwin_SINGLE_SHLIB") != -1:
-                    return BLAME_DONT_CARE # NSS
-                elif obj.find("central/opt-obj") != -1: # XXX WRONG
-                    return BLAME_MOZILLA
-                elif obj == "./js":
-                    return BLAME_MOZILLA
-                elif obj.find("tracemonkey") != -1:
-                    return BLAME_MOZILLA
-                elif obj.find("/System/Library/") != -1:
-                    return BLAME_MAC_LIBRARIES
-                elif obj.find("/usr/lib/") != -1:
-                    return BLAME_MAC_LIBRARIES
-    return BLAME_UNKNOWN
-
 def prettyStack(error):
     r = ""
     stack = error.getElementsByTagName("stack")[0]
@@ -63,27 +28,29 @@ def prettyStack(error):
 
 def amiss(fn, ignoreLeaks):
     a = False
+
     try:
         dom = xml.dom.minidom.parse(fn)
     except xml.parsers.expat.ExpatError, e:
         print "Error parsing the valgrind log: " + str(e)
         return False
+
     for error in dom.getElementsByTagName("error"):
         kind = error.getElementsByTagName("kind")[0].firstChild.data
         if kind == "Leak_PossiblyLost":
             continue # many false positives, or system library leaks, or something.
         if kind == "Leak_DefinitelyLost" and ignoreLeaks:
-            continue # this is the easiest way to work around bug 102229. more sophisticated would be to scan for DNS leaks first.
-        b = blame(error)
-        if b == BLAME_MOZILLA or b == BLAME_UNKNOWN:
-            print "Blame: " + str(b)
-            # Apparently some Valgrind errors (e.g. leaks) give "xwhat" while others (e.g. UMR) give "what".  xwtf.
-            if len(error.getElementsByTagName("xwhat")) > 0:
-                print error.getElementsByTagName("xwhat")[0].getElementsByTagName("text")[0].firstChild.data
-            elif len(error.getElementsByTagName("what")) > 0:
-                print error.getElementsByTagName("what")[0].firstChild.data
-            print prettyStack(error)
-            a = True
+            continue # this is the easiest way to work around bug 102229. more sophisticated would be to scan for DNS leaks first, or use the known-leak list.
+
+        # Apparently some Valgrind errors (e.g. leaks) give "xwhat" while others (e.g. UMR) give "what".  xwtf.
+        if len(error.getElementsByTagName("xwhat")) > 0:
+            print error.getElementsByTagName("xwhat")[0].getElementsByTagName("text")[0].firstChild.data
+        elif len(error.getElementsByTagName("what")) > 0:
+            print error.getElementsByTagName("what")[0].firstChild.data
+        print prettyStack(error)
+
+        a = True
+
     dom.unlink()
     return a
 
