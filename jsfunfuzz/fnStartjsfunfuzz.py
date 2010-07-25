79e60e6b80520f82f3aa4d9806667014ed507778
Gary Kwong <gary@rumblingedge.com>
2010-07-26 02:30:04 +0800

    So silent compilation does not yet work on pymake. Hmmmm...

--- jsfunfuzz/fnStartjsfunfuzz.py
+++ jsfunfuzz/fnStartjsfunfuzz.py
@@ -213,7 +213,7 @@ def compileCopy(archNum, compileType, branchType, usePymake):
     This function compiles and copies a binary.
     '''
     # Run make using 2 cores, not sure if pymake allows parallel compilation yet.
-    subprocess.call(['python', '-O', '../../build/pymake/make.py', '-j2', '-s']) if usePymake \
+    subprocess.call(['python', '-O', '../../build/pymake/make.py', '-j2']) if usePymake \
         else subprocess.call(['make', '-j2', '-s'])
     # Sniff platform and rename executable accordingly:
     if os.name == 'posix':
