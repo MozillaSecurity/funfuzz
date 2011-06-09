63b1e1c2620e545f1beaf21b76c9c3685016933e
Jesse Ruderman <jruderman@gmail.com>
2011-06-09 01:06:02 -0700

    Remove workarounds bug 622315 (fixed)

--- dom/automation/rundomfuzz.py
+++ dom/automation/rundomfuzz.py
@@ -170,9 +170,6 @@ class AmissLogHandler:
       self.printAndLog("@@@ " + msg)
     if msg.find("###!!! ASSERTION") != -1:
       self.nsassertionCount += 1
-      if msg.find("Foreground URLs are active") != -1 or msg.find("Entry added to loadgroup twice") != -1:
-        print "Ignoring memory leaks (bug 622315)" # testcase in comment 2
-        self.expectedToLeak = True
       if self.nsassertionCount == 100:
         print "rundomfuzz.py: not considering it a failure if browser hangs, because assertions are slow with stack-printing on. Please test in opt builds too, or fix the assertion bugs."
         self.expectedToHang = True
