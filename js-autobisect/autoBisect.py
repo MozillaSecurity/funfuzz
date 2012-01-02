336ada22cf1506961bfb4245e2c8de728047bd8b
Gary Kwong <gary@rumblingedge.com>
2012-01-02 11:43:35 -0800

    Change earliest compilable Lion changeset.

--- js-autobisect/autoBisect.py
+++ js-autobisect/autoBisect.py
@@ -433,7 +433,7 @@ def earliestKnownWorkingRev(flagsRequired, archNum, valgrindSupport):
     elif os.name == 'nt':
         return 'ea59b927d99f' # 46436 on m-c, first rev that can run pymake on Windows with most recent set of instructions
     elif isLion:
-        return "1a44373ccaf6" # 32315 on m-c, config.guess change for Lion
+        return "0485355df13d" # 78875 on m-c, one of the first stable revs to compile on Lion
     elif isSL and archNum == "64":
         return "1a44373ccaf6" # 32315 on m-c, config.guess change for snow leopard
     elif (os.uname()[0] == 'Linux') or (isSL and archNum == "32"):
