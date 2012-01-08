6b4dd6209104c91060b4700ebfbf4423258fc020
Gary Kwong <gary@rumblingedge.com>
2012-01-07 18:30:50 -0800

    Move to MPL 2.

--- jsfunfuzz/startjsfunfuzz.py
+++ jsfunfuzz/startjsfunfuzz.py
@@ -1,36 +1,8 @@
 #!/usr/bin/env python
 #
-#/* ***** BEGIN LICENSE BLOCK	****
-# Version: MPL 1.1/GPL 2.0/LGPL 2.1
-#
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is jsfunfuzz.
-#
-# The Initial Developer of the Original Code is
-# Gary Kwong.
-# Portions created by the Initial Developer are Copyright (C) 2008-2012
-# the Initial Developer. All Rights Reserved.
-#
-# Contributor(s):
-#
-# Alternatively, the contents of this file may be used under the terms of
-# either the GNU General Public License Version 2 or later (the "GPL"), or
-# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case the provisions of the GPL or the LGPL are applicable instead
-# of those above. If you wish to allow use of your version of this file only
-# the provisions above, a recipient may use your version of this file under
-# the terms of any one of the MPL, the GPL or the LGPL.
-#
-# * ***** END LICENSE BLOCK	****	/
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
 import platform
