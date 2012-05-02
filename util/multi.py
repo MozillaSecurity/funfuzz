3b0f92144ab4d3e7f0852d6ecb1196c197f82197
Jesse Ruderman <jruderman@gmail.com>
2012-05-02 16:07:00 -0700

    Use os.devnull

--- util/multi.py
+++ util/multi.py
@@ -5,15 +5,12 @@ import subprocess
 import sys
 import time
 
-WIN = (platform.system() in ("Microsoft", "Windows"))
-DEV_NULL = 'NUL' if WIN else '/dev/null'
-
 if __name__ == "__main__":
     count = int(sys.argv[1])
     command = sys.argv[2:]
     close_fds = sys.platform != 'win32'
     for i in range(count):
-        subprocess.Popen(command, close_fds=close_fds, stdout=open(DEV_NULL, 'w'), stderr=subprocess.STDOUT)
+        subprocess.Popen(command, close_fds=close_fds, stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
     # Leave this process running, in order to allow Ctrl+C to kill em all
     while True:
         time.sleep(10)
