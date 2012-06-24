6bcd795da37ef2c4c3779594ca8d386bd7bbed3b
Jesse Ruderman <jruderman@gmail.com>
2012-06-24 15:35:16 -0700

    Fix indentation again

--- js/jsInteresting.py
+++ js/jsInteresting.py
@@ -143,18 +143,18 @@ def valgrindSuppressions(knownPath):
     return a
 
 def deleteLogs(logPrefix):
-      """Whoever calls baseLevel should eventually call deleteLogs (unless a bug was found)."""
+    """Whoever calls baseLevel should eventually call deleteLogs (unless a bug was found)."""
     # If this turns up a WindowsError on Windows, remember to have excluded fuzzing locations in
     # the search indexer, anti-virus realtime protection and backup applications.
-     os.remove(logPrefix + "-out")
-     os.remove(logPrefix + "-err")
-     if (os.path.exists(logPrefix + "-crash")):
-         os.remove(logPrefix + "-crash")
-     if (os.path.exists(logPrefix + "-vg.xml")):
-         os.remove(logPrefix + "-vg.xml")
-     # FIXME: in some cases, subprocesses.py gzips a core file only for us to delete it immediately.
-     if (os.path.exists(logPrefix + "-core.gz")):
-         os.remove(logPrefix + "-core.gz")
+    os.remove(logPrefix + "-out")
+    os.remove(logPrefix + "-err")
+    if (os.path.exists(logPrefix + "-crash")):
+        os.remove(logPrefix + "-crash")
+    if (os.path.exists(logPrefix + "-vg.xml")):
+        os.remove(logPrefix + "-vg.xml")
+    # FIXME: in some cases, subprocesses.py gzips a core file only for us to delete it immediately.
+    if (os.path.exists(logPrefix + "-core.gz")):
+        os.remove(logPrefix + "-core.gz")
 
 def parseOptions(args):
     parser = OptionParser()
