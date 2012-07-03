e6f50f7d7cbd34e6b66f2f37a63e7faa98f4a45d
Gary Kwong <gary@rumblingedge.com>
2012-07-03 13:03:23 -0700

    Attribute countCpus.py.

--- util/countCpus.py
+++ util/countCpus.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this file,
-# You can obtain one at http://mozilla.org/MPL/2.0/.
-
 import os
 
 def cpuCount():
     '''
     A version of cpu_count() that seems compatible with Python 2.5
+    Adapted from http://codeliberates.blogspot.com/2008/05/detecting-cpuscores-in-python.html
     '''
     # POSIX platforms
     if hasattr(os, 'sysconf'):
