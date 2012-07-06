0a7ed8b374a551d2c0850a47b9e2bd9452671d0f
Jesse Ruderman <jruderman@gmail.com>
2012-07-06 02:05:41 -0700

    Ignore another case of bug 646599

--- js/jsfunfuzz.js
+++ js/jsfunfuzz.js
@@ -226,6 +226,7 @@ function whatToTestSpidermonkeyTrunk(code)
       && !( codeL.match( /\(.*\).*\(.*\)/ ))          // parenthesized callee expression (bug 646695, etc)
       && !( codeL.match( /new.*\(.*\)/ ))             // parenthesized callee expression (bug 646695, etc)
       && !( codeL.match( /\[.*\+/ ))        // constant folding bug 646599
+      && !( codeL.match( /\[.*\?/ ))        // constant folding bug 646599
       && (code.indexOf("*") == -1)         // constant folding bug 539819
       && (code.indexOf("/") == -1)         // constant folding bug 539819
       && (code.indexOf("default") == -1)   // avoid bug 355509
