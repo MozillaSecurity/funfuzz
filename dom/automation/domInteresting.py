bcf042a19cc832c526c24fc49e5e3ff1fd38a02f
Jesse Ruderman <jruderman@gmail.com>
2012-09-19 10:50:27 -0700

    Remove fixed bugs

--- dom/automation/domInteresting.py
+++ dom/automation/domInteresting.py
@@ -299,7 +299,6 @@ class AmissLogHandler:
              msg.find("prompt aborted by user") == -1 and # thrown intentionally in nsPrompter.js
              msg.find("nsIIOService.getProtocolHandler") == -1 and # bug 746878
              msg.find("tipElement is null") == -1 and # bug 746893
-             msg.find("aboutHome.js") == -1 # bug 615785
             ):
             self.printAndLog("@@@ " + msg)
             self.sawChromeFailure = True
