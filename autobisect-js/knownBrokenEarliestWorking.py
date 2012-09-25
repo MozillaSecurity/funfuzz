5ace518f32e50beb2064e4e763be8fbbc28fc99d
Gary Kwong <gary@rumblingedge.com>
2012-09-25 00:28:02 -0700

    Rename from fList to flags in knownBrokenEarliestWorking.py.

--- autobisect-js/knownBrokenEarliestWorking.py
+++ autobisect-js/knownBrokenEarliestWorking.py
@@ -56,37 +56,37 @@ def knownBrokenRanges():
 def earliestKnownWorkingRev(options):
     """Returns the oldest version of the shell that can run jsfunfuzz."""
     assert (not isMac) or (macVer() >= [10, 7])  # Only Lion and above are supported with Clang 4.
-    fList = options.flagsReqList
+    flags = options.paramList
 
     # These should be in descending order, or bisection will break at earlier changesets.
     # See 7aba0b7a805f, 98725 on m-c, for first stable root analysis builds
-    if options.raSupport or options.isThreadsafe:  # Threadsafety result is wrong prior to this rev
+    if options.enableRootAnalysis or options.isThreadsafe:  # Threadsafety result is wrong prior to this rev
         return 'e3799f9cfee8' # 107071 on m-c, first rev with correct getBuildConfiguration details
-    elif '--no-ti' in fList or '--no-ion' in fList or '--no-jm' in fList:
+    elif '--no-ti' in flags or '--no-ion' in flags or '--no-jm' in flags:
         return '300ac3d58291' # 106120 on m-c, See bug 724751: IonMonkey flag change
-    elif '--ion' in fList:
+    elif '--ion' in flags:
         return '43b55878da46' # 105662 on m-c, IonMonkey's approximate first stable rev w/ --ion -n
-    elif '--ion-eager' in fList:  # See bug 683039 - Delay Ion compilation until a function is hot
+    elif '--ion-eager' in flags:  # See bug 683039 - Delay Ion compilation until a function is hot
         return '4ceb3e9961e4' # 105173 on m-c
     elif isMac and macVer() >= [10, 7]:
         return '2046a1f46d40' # 87022 on m-c, first rev that compiles well on Mac under Clang
-    elif '-n' in fList and ('-D' in fList or '--dump-bytecode' in fList):
+    elif '-n' in flags and ('-D' in flags or '--dump-bytecode' in flags):
         return '0c5ed245a04f' # 75176 on m-c, merge brings in -D from one side and -n from another
-    elif '-n' in fList:
+    elif '-n' in flags:
         return '228e319574f9' # 74704 on m-c, first rev that has the -n option
-    elif '--debugjit' in fList or '--methodjit' in fList or '--dump-bytecode' in fList:
+    elif '--debugjit' in flags or '--methodjit' in flags or '--dump-bytecode' in flags:
         return 'b1923b866d6a' # 73054 on m-c, first rev that has long variants of many options
-    elif '-D' in fList:
+    elif '-D' in flags:
         return 'e5b92c2bdd2d' # 70991 on m-c, first rev that has the -D option
-    elif '-a' in fList:  # -a only works with -m
+    elif '-a' in flags:  # -a only works with -m
         return 'f569d49576bb' # 62574 on m-c, first rev that has the -a option
-    elif '-p' in fList:
+    elif '-p' in flags:
         return '339457364540' # 56551 on m-c, first rev that has the -p option
-    elif '-d' in fList:  # To bisect farther back, use setDebug(true). See bug 656381 comment 0.
+    elif '-d' in flags:  # To bisect farther back, use setDebug(true). See bug 656381 comment 0.
         return 'ea0669bacf12' # 54578 on m-c, first rev that has the -d option
-    elif isWin and '-m' in fList:
+    elif isWin and '-m' in flags:
         return '9f2641871ce8' # 53544 on m-c, first rev that can run with pymake and -m
-    elif '-m' in fList:
+    elif '-m' in flags:
         return '547af2626088' # 53105 on m-c, first rev that can run jsfunfuzz-n.js with -m
     elif isWin:
         return 'ea59b927d99f' # 46436 on m-c, first rev that can run pymake on Windows
