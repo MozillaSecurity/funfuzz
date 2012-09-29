6f6f24df6daab3679a1d0fa4dc855ed226dd68cd
Gary Kwong <gary@rumblingedge.com>
2012-09-28 21:35:20 -0700

    Unbreak pinpoint.py and autoBisect.py when Valgrind is enabled.

--- js/pinpoint.py
+++ js/pinpoint.py
@@ -32,7 +32,7 @@ def pinpoint(itest, logPrefix, jsEngine, engineFlags, infilename, bisectRepo, ta
     """
 
     valgrindSupport = "--valgrind" in itest
-    valgrindX = ["--enable-valgrind"] if valgrindSupport else []
+    valgrindX = ["--test-with-valgrind"] if valgrindSupport else []
 
     lithArgs = itest + [jsEngine] + engineFlags + [infilename]
     (lithResult, lithDetails) = lithOps.runLithium(lithArgs, logPrefix + "-1-lines", targetTime)
