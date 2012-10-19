b7f1956af8e024284d1b55c5ece768490b9dd221
Jesse Ruderman <jruderman@gmail.com>
2012-10-19 08:06:27 -0700

    Ensure that fuzzValues.texts returns a string. (In case fuzzerMixLists decides to mess with us.)

--- bot.py
+++ bot.py
@@ -372,6 +372,7 @@ def skipJobNamed(j):
     return (
         "b826f91e0c8511e280083c0754725936" in j or # Bug 797065
         "e9a41a6b16c311e280033c0754725936" in j or # Bug 801914, in its manifestation as a "hang"
+        "623986e119f511e280063c0754724cee" in j or # A fixed bug in the fuzzer
         "1339201819" in j or # Bug 763126
         "1338835174" in j or # Bug 763126
         "1339379020" in j or # Bug 763560
