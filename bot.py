f6a0b958e3b86cec7af10239f5c0701cde398db8
Gary Kwong <gary@rumblingedge.com>
2013-03-24 19:27:48 -0700

    Make bot.py also print the hgrc file of the repository, for informational purposes.

--- bot.py
+++ bot.py
@@ -260,6 +260,11 @@ def main():
         print "Python version: " + sys.version[:5]
         print "Number of cores visible to OS: " +  str(cpuCount())
         print 'Free space (GB): ' + str('%.2f') % getFreeSpace('/', 3)
+        print 'The hgrc of this repository is:'
+        with open(os.path.join('.hg', 'hgrc'), 'rb') as f:
+            hgrcContentList = f.readlines()
+        for line in hgrcContentList:
+            print line,
         if os.name == 'posix':
             # resource library is only applicable to Linux or Mac platforms.
             import resource
