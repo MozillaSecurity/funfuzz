3234ac42ebf94ff98f82ef232512ab38cdf7fb70
Gary Kwong <gary@rumblingedge.com>
2013-04-09 17:05:47 -0700

    Start moving away from localjsfunfuzz.py by moving major functions away from it and into bot.py

--- js/localjsfunfuzz.py
+++ js/localjsfunfuzz.py
@@ -8,22 +8,19 @@ from __future__ import with_statement
 
 import os
 import platform
-import shutil
 import subprocess
 import sys
 from optparse import OptionParser
-from tempfile import mkdtemp
 
 import buildOptions
-from compileShell import CompiledShell, cfgJsCompileCopy, copyJsSrcDirs
-from inspectShell import verifyBinary
 
 path0 = os.path.dirname(os.path.abspath(__file__))
 path1 = os.path.abspath(os.path.join(path0, os.pardir, 'util'))
 sys.path.append(path1)
-from hgCmds import getRepoHashAndId, getRepoNameFromHgrc, patchHgRepoUsingMq
-from lithOps import knownBugsDir
-from subprocesses import captureStdout, dateStr, normExpUserPath, shellify, vdump
+path2 = os.path.abspath(os.path.join(path0, os.pardir))
+sys.path.append(path2)
+from bot import localCompileFuzzJsShell
+from subprocesses import normExpUserPath
 
 def machineTimeoutDefaults(options):
     '''Sets different defaults depending on the machine type or debugger used.'''
@@ -87,126 +84,6 @@ def parseOptions():
     options.timeout = options.timeout or machineTimeoutDefaults(options)
 
     return options
-
-def envDump(shell, log):
-    '''Dumps environment to file.'''
-    with open(log, 'ab') as f:
-        f.write('Information about shell:\n\n')
-
-        f.write('Create another shell in autobisect-cache like this one:\n')
-        f.write(shellify(["python", "-u", os.path.join(path0, "compileShell.py"),
-            "-R", shell.getRepoDir(), "-b", shell.buildOptions.inputArgs]) + "\n\n")
-
-        f.write('Full environment is: ' + str(shell.getEnvFull()) + '\n')
-        f.write('Environment variables added are:\n')
-        f.write(shellify(shell.getEnvAdded()) + '\n\n')
-
-        f.write('Configuration command was:\n')
-        f.write(shellify(shell.getCfgCmdExclEnv()) + '\n\n')
-
-        f.write('Full configuration command with needed environment variables is:\n')
-        f.write(shellify(shell.getEnvAdded()) + ' ' + shellify(shell.getCfgCmdExclEnv()) + '\n\n')
-
-def cmdDump(shell, cmdList, log):
-    '''Dump commands to file.'''
-    with open(log, 'ab') as f:
-        f.write('Command to be run is:\n')
-        f.write(shellify(cmdList) + '\n')
-        f.write('========================================================\n')
-        f.write('|  Fuzzing %s js shell builds\n' %
-                     (shell.getRepoName() ))
-        f.write('|  DATE: %s\n' % dateStr())
-        f.write('========================================================\n\n')
-
-def localCompileFuzzJsShell(options):
-    '''Compiles and readies a js shell for fuzzing.'''
-    print dateStr()
-    localOrigHgHash, localOrigHgNum, isOnDefault = getRepoHashAndId(options.buildOptions.repoDir)
-
-    # Assumes that all patches that need to be applied will be done through --enable-patch-dir=FOO.
-    assert captureStdout(['hg', '-R', options.buildOptions.repoDir, 'qapp'])[0] == ''
-
-    if options.patchDir:  # Note that only JS patches are supported, not NSPR.
-        # Assume mq extension enabled. Series file should be optional if only one patch is needed.
-        assert not os.path.isdir(options.patchDir), \
-            'Support for multiple patches has not yet been added.'
-        assert os.path.isfile(options.patchDir)
-        p1name = patchHgRepoUsingMq(options.patchDir, options.buildOptions.repoDir)
-
-    appendStr = ''
-    if options.patchDir:
-        appendStr += '-patched'
-    userDesktopFolder = normExpUserPath(os.path.join('~', 'Desktop'))
-    if not os.path.isdir(userDesktopFolder):
-        try:
-            os.mkdir(userDesktopFolder)
-        except OSError:
-            raise Exception('Unable to create ~/Desktop folder.')
-    # WinXP has spaces in the user directory.
-    fuzzResultsDirStart = 'c:\\' if platform.uname()[2] == 'XP' else userDesktopFolder
-    buildIdentifier = '-'.join([getRepoNameFromHgrc(options.buildOptions.repoDir), localOrigHgNum, \
-        localOrigHgHash])
-    fullPath = mkdtemp(appendStr + os.sep,
-                       buildOptions.computeShellName(options.buildOptions, buildIdentifier) + "-",
-                       fuzzResultsDirStart)
-    vdump('Base temporary directory is: ' + fullPath)
-
-    myShell = CompiledShell(options.buildOptions, localOrigHgHash, fullPath)
-
-    # Copy js src dirs to compilePath, to have a backup of shell source in case repo gets updated.
-    copyJsSrcDirs(myShell)
-
-    if options.patchDir:
-        # Remove the patches from the codebase if they were applied.
-        assert not os.path.isdir(options.patchDir), \
-            'Support for multiple patches has not yet been added.'
-        assert p1name != ''
-        if os.path.isfile(options.patchDir):
-            subprocess.check_call(['hg', '-R', myShell.getRepoDir(), 'qpop'])
-            vdump("First patch qpop'ed.")
-            subprocess.check_call(['hg', '-R', myShell.getRepoDir(), 'qdelete', p1name])
-            vdump("First patch qdelete'd.")
-
-    # Ensure there is no applied patch remaining in the main repository.
-    assert captureStdout(['hg', '-R', myShell.getRepoDir(), 'qapp'])[0] == ''
-
-    # Compile the shell to be fuzzed and verify it.
-    cfgJsCompileCopy(myShell, options.buildOptions)
-    verifyBinary(myShell, options.buildOptions)
-
-    analysisPath = os.path.abspath(os.path.join(path0, os.pardir, 'jsfunfuzz', 'analysis.py'))
-    if os.path.exists(analysisPath):
-        shutil.copy2(analysisPath, fullPath)
-
-    # Construct a command-line for running loopjsfunfuzz.py
-    cmdList = ['python', '-u']
-    cmdList.append(normExpUserPath(os.path.join(path0, 'loopjsfunfuzz.py')))
-    cmdList.append('--repo=' + myShell.getRepoDir())
-    cmdList += ["--build", options.buildOptions.inputArgs]
-    if options.buildOptions.runWithVg:
-        cmdList.append('--valgrind')
-    if not options.disableCompareJit:
-        cmdList.append('--comparejit')
-    if not options.disableRndFlags:
-        cmdList.append('--random-flags')
-    cmdList.append(str(options.timeout))
-    cmdList.append(knownBugsDir(myShell.getRepoName()))
-    cmdList.append(myShell.getShellBaseTempDirWithName())
-
-    # Write log files describing configuration parameters used during compilation.
-    localLog = normExpUserPath(os.path.join(myShell.getBaseTempDir(), 'log-localjsfunfuzz.txt'))
-    envDump(myShell, localLog)
-    cmdDump(myShell, cmdList, localLog)
-
-    with open(localLog, 'rb') as f:
-        for line in f:
-            if 'Full environment is' not in line:
-                print line,
-
-    # FIXME: Randomize logic should be developed later, possibly together with target time in
-    # loopjsfunfuzz.py. Randomize Valgrind runs too.
-
-    return myShell, cmdList
 
 def main():
     options = parseOptions()
