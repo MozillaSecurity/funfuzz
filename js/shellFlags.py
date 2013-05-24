38dffedf71a9b189fd7c8493d255b23c76c4d465
Gary Kwong <gary@rumblingedge.com>
2013-05-23 22:29:13 -0700

    Something else is broken with enableSPSProfilingAssertions again..

--- js/shellFlags.py
+++ js/shellFlags.py
@@ -92,7 +92,8 @@ def randomFlagSet(shellPath):
         args.append("--no-ion")
 
     # This is here because of bug 830508
-    if shellSupportsFlag(shellPath, "--execute=enableSPSProfilingAssertions(true)") and chance(.5):
+    # This will all be Falsed due to bug 874687, which breaks jsfunfuzz
+    if False and shellSupportsFlag(shellPath, "--execute=enableSPSProfilingAssertions(true)") and chance(.5):
         if chance(.5):
             args.append("--execute=enableSPSProfilingAssertions(true)")
         else:
