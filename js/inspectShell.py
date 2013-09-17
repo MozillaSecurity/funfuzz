3df06ee0e6c2d45790e8c52bf3194dbce9a84dcf
Gary Kwong <gary@rumblingedge.com>
2013-09-17 12:06:04 +0200

    Add extra Valgrind flag due to bug 913876 comment 18.

--- js/inspectShell.py
+++ js/inspectShell.py
@@ -64,6 +64,8 @@ def constructVgCmdList(errorCode=77):
         vgCmdList.append('--dsymutil=yes')
     vgCmdList.append('--error-exitcode=' + str(errorCode))
     vgCmdList.append('--smc-check=all-non-file')
+    # See bug 913876 comment 18:
+    vgCmdList.append('--vex-iropt-register-updates=allregs-at-mem-access')
     vgCmdList.append('--partial-loads-ok=yes')  # See bug 913883 comment 3
     vgCmdList.append('--gen-suppressions=all')
     vgCmdList.append('--leak-check=full')
