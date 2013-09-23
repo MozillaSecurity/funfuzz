1cea08287df41a951efa3600e72cd0cbba74a053
Jesse Ruderman <jruderman@gmail.com>
2013-09-22 19:44:43 -0700

    Ignore bug 919362

--- dom/automation/domInteresting.py
+++ dom/automation/domInteresting.py
@@ -378,6 +378,7 @@ def knownChromeFailure(msg):
         ("browser.js" in msg and "this.UIModule is undefined" in msg) or  # Bug 877013
         ("browser.js" in msg and "this._cps2 is undefined" in msg) or     # Bug 877013
         ("browser.js" in msg and "this.button is null" in msg) or         # Bug 877013
+        ("browser.xml" in msg and "this.docShell is null" in msg) or      # Bug 919362
         ("places.js" in msg and "PlacesUIUtils is not defined" in msg) or # Bug 801436
         ("places.js" in msg and "this._places is null" in msg) or         # Bug 893322
         ("pageInfo.js" in msg and "elem.ownerDocument.defaultView" in msg) or # Bug 799329
