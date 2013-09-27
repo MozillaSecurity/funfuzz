e4af0cc958ee4dc0ef435708598b771f36bc5536
Jesse Ruderman <jruderman@gmail.com>
2013-09-26 20:14:12 -0700

    Add most of the prefs that Christoph uses for fuzzing, randomizing the feature-enabling prefs

--- dom/automation/constant-prefs.js
+++ dom/automation/constant-prefs.js
@@ -34,6 +34,7 @@ user_pref("browser.rights.3.shown", true);
 user_pref("browser.firstrun.show.localepicker", false);
 user_pref("browser.firstrun.show.uidiscovery", false);
 user_pref("browser.startup.page", 0); // use about:blank, not browser.startup.homepage
+user_pref("general.warnOnAboutConfig", false);
 //user_pref("datareporting.policy.dataSubmissionPolicyBypassAcceptance", true);
 
 // Suppress automatic safe mode after crashes.
@@ -61,3 +62,18 @@ user_pref("datareporting.healthreport.service.enabled", false);
 
 // Looking up the system proxy settings can cause problems (see bug 794174, see bug 793016)
 user_pref("network.proxy.type", 0);
+
+// Prefs from Christoph (?)
+user_pref("network.jar.open-unsafe-types", true);
+user_pref("gfx.color_management.mode", 2);
+user_pref("gfx.canvas.azure.backends", "skia"); //direct2d, skia, cairo
+user_pref("media.peerconnection.aec", 1);
+user_pref("media.peerconnection.aec_enabled", true);
+user_pref("media.peerconnection.agc", 1);
+user_pref("media.peerconnection.agc_enabled", false);
+user_pref("media.peerconnection.default_iceservers", '[{"url": "stun:23.21.150.121"}]');
+user_pref("media.peerconnection.noise", 1);
+user_pref("media.peerconnection.noise_enabled", false);
+user_pref("media.peerconnection.use_document_iceservers", true);
+user_pref("media.peerconnection.turn.disable", false);
+user_pref("browser.ssl_override_behavior", 1);
