b6f3ba0a1ca000359787f75591884910b38e7c1c
Gary Kwong <gary@rumblingedge.com>
2013-11-12 16:39:43 -0800

    Bug 912303 is now fixed.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -29,7 +29,6 @@ function whatToTestSpidermonkeyTrunk(code)
 
     expectConsistentOutputAcrossJITs: true
        && code.indexOf("'strict") == -1             // see bug 743425
-       && code.indexOf("__noSuchMethod__") == -1    // bug 912303
        && code.indexOf("gcPreserveCode") == -1      // bug 912328
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/)) // doesn't stay valid utf-8 after going through python (?)
 
