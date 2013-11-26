5e1c786b4747751b16ed02816295c82a6c4f6f55
Gary Kwong <gary@rumblingedge.com>
2013-11-26 12:30:14 -0800

    Backout 70f68eac1ed3, reverting us to using make again.

--- js/compileShell.py
+++ js/compileShell.py
@@ -461,9 +461,7 @@ def copyJsSrcDirs(shell):
 def compileJsCopy(shell, options):
     '''This function compiles and copies a binary.'''
     try:
-        # Bug 925605 adds a requirement for a specially-compiled make, so using pymake again instead
-        cmdList = ['python', '-OO', normExpUserPath(os.path.join(shell.getRepoDir(),
-                      'build', 'pymake', 'make.py')), '-j' + str(COMPILATION_JOBS), '-s']
+        cmdList = ['make', '-C', shell.getJsObjdir(), '-j' + str(COMPILATION_JOBS), '-s']
         out = captureStdout(cmdList, combineStderr=True, ignoreExitCode=True,
                             currWorkingDir=shell.getJsObjdir(), env=shell.getEnvFull())[0]
         if 'no such option: -s' in out:  # Retry only for this situation.
