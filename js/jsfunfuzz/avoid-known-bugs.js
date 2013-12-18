0b6409bfbee172dc564157402d4502f9091b3caa
Gary Kwong <gary@rumblingedge.com>
2013-12-17 19:45:07 -0800

    Bug 912328, or rather bug 932982 as it was duped to, has been fixed.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -30,7 +30,6 @@ function whatToTestSpidermonkeyTrunk(code)
 
     expectConsistentOutputAcrossJITs: true
        && code.indexOf("'strict") == -1             // see bug 743425
-       && code.indexOf("gcPreserveCode") == -1      // bug 912328
        && code.indexOf("Object.seal") == -1         // bug 937922
        && code.indexOf("scatterPar") == -1          // bug 944975
        && code.indexOf("RegExp") == -1              // bug 945512
