cb8414dc6a67c7831fff319eb8c5639fba393cd2
Jesse Ruderman <jruderman@gmail.com>
2014-01-09 15:34:49 -0800

    Add parens around expressions that call testing functions. This is needed to prevent the fuzzer from sticking in a 'new' operator, causing one branch but not the other of a isAsmJSCompilationAvailable trinary to throw '[built-in function] is not a constructor'.

--- js/shared/testing-functions.js
+++ js/shared/testing-functions.js
@@ -27,7 +27,7 @@ var fuzzTestingFunctions = (function(glob){
     var level = rnd(14);
     if (browser && level == 9) level = 0; // bug 815241
     var period = numberOfAllocs();
-    return tf("gczeal") + "(" + level + ", " + period + ")";
+    return "(" + tf("gczeal") + "(" + level + ", " + period + ")" + ")";
   }
 
   var testingFunctions = Random.weighted([
@@ -42,16 +42,16 @@ var fuzzTestingFunctions = (function(glob){
     { w: 20, v: function(d, b) { return "(void " + tf("minorgc") + "(true)" + ")"; } },
 
     // Invoke an incremental garbage collection slice.
-    { w: 20, v: function(d, b) { return tf("gcslice") + "(" + Math.floor(Math.pow(2, Random.float() * 32)) + ")"; } },
+    { w: 20, v: function(d, b) { return "(" + tf("gcslice") + "(" + Math.floor(Math.pow(2, Random.float() * 32)) + ")" + ")"; } },
 
     // Schedule the given objects to be marked in the next GC slice.
-    { w: 10, v: function(d, b) { return tf("selectforgc") + "(" + object(d, b) + ")"; } },
+    { w: 10, v: function(d, b) { return "(" + tf("selectforgc") + "(" + object(d, b) + ")" + ")"; } },
 
     // Add a compartment to the next garbage collection.
-    { w: 10, v: function(d, b) { return tf("schedulegc") + "(" + global(d, b) + ")"; } },
+    { w: 10, v: function(d, b) { return "(" + tf("schedulegc") + "(" + global(d, b) + ")" + ")"; } },
 
     // Schedule a GC for after N allocations.
-    { w: 10, v: function(d, b) { return tf("schedulegc") + "(" + numberOfAllocs() + ")"; } },
+    { w: 10, v: function(d, b) { return "(" + tf("schedulegc") + "(" + numberOfAllocs() + ")" + ")"; } },
 
     // Make garbage collection extremely frequent (SLOW)
     { w: 1,  v: function(d, b) { return (!browser || rnd(100) == 0) ? (enableGCZeal()) : "0"; } },
@@ -59,33 +59,33 @@ var fuzzTestingFunctions = (function(glob){
     // Verify write barriers. These functions are effective in pairs.
     // The first call sets up the start barrier, the second call sets up the end barrier.
     // Nothing happens when there is only one call.
-    { w: 10, v: function(d, b) { return tf("verifyprebarriers") + "()"; } },
-    { w: 10, v: function(d, b) { return tf("verifypostbarriers") + "()"; } },
+    { w: 10, v: function(d, b) { return "(" + tf("verifyprebarriers") + "()" + ")"; } },
+    { w: 10, v: function(d, b) { return "(" + tf("verifypostbarriers") + "()" + ")"; } },
 
     // Trace the heap using non-GC tracing code
     { w: 1,  v: function(d, b) { return "(void " + tf("countHeap") + "()" + ")"; } },
 
     // Toggle various validations.
-    { w: 5,  v: function(d, b) { return tf("validategc") + "(false)"; } },
-    { w: 1,  v: function(d, b) { return tf("validategc") + "(true)"; } },
-    { w: 5,  v: function(d, b) { return tf("fullcompartmentchecks") + "(false)"; } },
-    { w: 1,  v: function(d, b) { return tf("fullcompartmentchecks") + "(true)"; } },
-    { w: 5,  v: function(d, b) { return tf("setIonCheckGraphCoherency") + "(false)"; } },
-    { w: 1,  v: function(d, b) { return tf("setIonCheckGraphCoherency") + "(true)"; } },
-    { w: 1,  v: function(d, b) { return tf("enableOsiPointRegisterChecks") + "()"; } },
+    { w: 5,  v: function(d, b) { return "(" + tf("validategc") + "(false)" + ")"; } },
+    { w: 1,  v: function(d, b) { return "(" + tf("validategc") + "(true)" + ")"; } },
+    { w: 5,  v: function(d, b) { return "(" + tf("fullcompartmentchecks") + "(false)" + ")"; } },
+    { w: 1,  v: function(d, b) { return "(" + tf("fullcompartmentchecks") + "(true)" + ")"; } },
+    { w: 5,  v: function(d, b) { return "(" + tf("setIonCheckGraphCoherency") + "(false)" + ")"; } },
+    { w: 1,  v: function(d, b) { return "(" + tf("setIonCheckGraphCoherency") + "(true)" + ")"; } },
+    { w: 1,  v: function(d, b) { return "(" + tf("enableOsiPointRegisterChecks") + "()" + ")"; } },
 
     // Run-time equivalents to --baseline-eager or --baseline-uses-before-compile, --no-baseline, etc
-    { w: 1,  v: function(d, b) { return tf("setJitCompilerOption") + "('baseline.usecount.trigger', " + rnd(20) + ")"; } },
-    { w: 1,  v: function(d, b) { return tf("setJitCompilerOption") + "('ion.usecount.trigger', " + rnd(40) + ")"; } },
+    { w: 1,  v: function(d, b) { return "(" + tf("setJitCompilerOption") + "('baseline.usecount.trigger', " + rnd(20) + ")" + ")"; } },
+    { w: 1,  v: function(d, b) { return "(" + tf("setJitCompilerOption") + "('ion.usecount.trigger', " + rnd(40) + ")" + ")"; } },
     //{ w: 1,  v: function(d, b) { return tf("setJitCompilerOption") + "('ion.enable', " + rnd(2) + ")"; } }, // see bug 949807
     //{ w: 1,  v: function(d, b) { return tf("setJitCompilerOption") + "('baseline.enable', " + rnd(2) + ")"; } }, // bug 932284
 
     // I'm not sure what this does in the shell.
-    { w: 5,  v: function(d, b) { return tf("deterministicgc") + "(false)"; } },
-    { w: 1,  v: function(d, b) { return tf("deterministicgc") + "(true)"; } },
+    { w: 5,  v: function(d, b) { return "(" + tf("deterministicgc") + "(false)" + ")"; } },
+    { w: 1,  v: function(d, b) { return "(" + tf("deterministicgc") + "(true)" + ")"; } },
 
     // Causes JIT code to always be preserved by GCs afterwards (see https://bugzilla.mozilla.org/show_bug.cgi?id=750834)
-    { w: 5,  v: function(d, b) { return tf("gcPreserveCode") + "()"; } },
+    { w: 5,  v: function(d, b) { return "(" + tf("gcPreserveCode") + "()" + ")"; } },
   ]);
 
   return { testingFunctions: testingFunctions, enableGCZeal: enableGCZeal }
