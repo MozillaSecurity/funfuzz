05a70dd754066bedfcd6ddebc992263201199af3
Gary Kwong <gary@rumblingedge.com>
2014-04-19 11:56:21 -0700

    Add bug 998709.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -41,6 +41,7 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("defineProperty") == -1      // bug 998059
        && code.indexOf("buildPar") == -1            // bug 998262
        && code.indexOf("with") == -1                // bug 998580
+       && code.indexOf("__proto__") == -1           // bug 998709
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/)) // doesn't stay valid utf-8 after going through python (?)
 
   };
