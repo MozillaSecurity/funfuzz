a88f7da50e2a2ed30726420f76f96cbf0c18aaf7
Gary Kwong <gary@rumblingedge.com>
2014-06-24 02:26:28 -0700

    Add support for --latin1-strings if available.

--- js/shellFlags.py
+++ js/shellFlags.py
@@ -36,6 +36,9 @@ def randomFlagSet(shellPath):
     if shellSupportsFlag(shellPath, '--fuzzing-safe'):
         args.append("--fuzzing-safe")  # --fuzzing-safe landed in bug 885361
 
+    if shellSupportsFlag(shellPath, '--latin1-strings') and chance(.2):
+        args.append("--latin1-strings")  # See bug 1028867
+
     if queryBuildConfiguration(shellPath, 'arm-simulator') and chance(.4):
         args.append('--arm-sim-icache-checks')
 
