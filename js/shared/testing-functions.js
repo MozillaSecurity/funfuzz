e1498060eb4ee7e2833a12ddd49c7228aa0ae723
Jesse Ruderman <jruderman@gmail.com>
2014-06-30 22:06:47 -0700

    Remove workarounds for FIXED bugs

--- js/shared/testing-functions.js
+++ js/shared/testing-functions.js
@@ -99,8 +99,8 @@ var fuzzTestingFunctions = (function(glob){
 
     // Toggle the built-in profiler.
     { w: 1,  v: function(d, b) { return "(" + tf("enableSPSProfiling") + "()" + ")"; } },
-    { w: 1,  v: function(d, b) { return "(" + "void" + "(" + tf("enableSPSProfilingWithSlowAssertions") + "()" + ")" + ")"; } }, // void -- bug 1007187
-    { w: 5,  v: function(d, b) { return "(" + "void" + "(" + tf("disableSPSProfiling") + "()" + ")" + ")"; } }, // void -- bug 1006894
+    { w: 1,  v: function(d, b) { return "(" + tf("enableSPSProfilingWithSlowAssertions") + "()" + ")"; } },
+    { w: 5,  v: function(d, b) { return "(" + tf("disableSPSProfiling") + "()" + ")"; } },
 
     // I'm not sure what this does in the shell.
     { w: 5,  v: function(d, b) { return "(" + tf("deterministicgc") + "(false)" + ")"; } },
