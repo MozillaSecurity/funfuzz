880cfc7027740f0c3d84ac9f869bfce0b0a5ef1f
Gary Kwong <gary@rumblingedge.com>
2014-07-21 10:37:14 -1000

    Bug 1037665 is now fixed.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -42,7 +42,6 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("__proto__") == -1           // bug 1033873
        && code.indexOf("exec") == -1                // bug 1033946
        && code.indexOf("RegExp") == -1              // bug 1033946
-       && code.indexOf("arguments.callee") == -1    // bug 1037665
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/)) // doesn't stay valid utf-8 after going through python (?)
 
   };
