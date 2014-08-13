f64c1c87cff2b592ae6a0941324dd537b3a70047
Gary Kwong <gary@rumblingedge.com>
2014-08-13 13:56:42 -0700

    Add --ion-loop-unrolling=on from bug 1039458. Thanks Jan.

--- js/shellFlags.py
+++ js/shellFlags.py
@@ -43,6 +43,9 @@ def randomFlagSet(shellPath):
     if shellSupportsFlag(shellPath, '--fuzzing-safe'):
         args.append("--fuzzing-safe")  # --fuzzing-safe landed in bug 885361
 
+    if shellSupportsFlag(shellPath, '--ion-loop-unrolling=on') and chance(.2):
+        args.append("--ion-loop-unrolling=on")  # --ion-loop-unrolling=on landed in bug 1039458
+
     if shellSupportsFlag(shellPath, '--no-threads') and chance(.5):
         args.append("--no-threads")  # --no-threads landed in bug 1031529
 
