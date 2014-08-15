4777bf99c502d9991930d8052cc5cf6d717ecd5a
Gary Kwong <gary@rumblingedge.com>
2014-08-15 15:31:32 -0700

    Add bug 1054545.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -42,6 +42,7 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("exec") == -1                // bug 1033946
        && code.indexOf("RegExp") == -1              // bug 1033946
        && code.indexOf("buildPar") == -1            // bug 1054531
+       && code.indexOf("enumerable") == -1          // bug 1054545
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/)) // doesn't stay valid utf-8 after going through python (?)
 
   };
