68e6a106f86e8c422068d2a75d6a928cfd6c8bbd
Gary Kwong <gary@rumblingedge.com>
2015-01-20 14:32:21 -0800

    Add known broken range for SpiderMonkey.

--- autobisect-js/knownBrokenEarliestWorking.py
+++ autobisect-js/knownBrokenEarliestWorking.py
@@ -61,6 +61,7 @@ def knownBrokenRanges(options):
         hgrange('03242a11d044', '31714af41f2c'), # Fx35, broken spidermonkey due to let changes
         hgrange('b160657339f8', '06d07689a043'), # Fx36, unstable spidermonkey
         hgrange('1c9c64027cac', 'ef7a85ec6595'), # Fx37, unstable spidermonkey
+        hgrange('7c25be97325d', 'd426154dd31d'), # Fx38, broken spidermonkey
     ]
 
     if sps.isARMv7l:
