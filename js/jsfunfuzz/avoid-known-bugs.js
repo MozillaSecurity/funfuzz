0f41007dfaac9d03f0f5cf37523911c42240f92e
Gary Kwong <gary@rumblingedge.com>
2015-02-02 14:36:10 -0800

    Bug 1122402 and bug 1124036 are now fixed.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -39,12 +39,11 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("preventExtensions") == -1   // bug 1085299
        && code.indexOf("Math.round") == -1          // bug 1122344
        && code.indexOf("Math.ceil") == -1           // bug 1122401
-       && code.indexOf("0x80000000") == -1          // bug 1122402
-       && code.indexOf("Math.max") == -1            // bug 1122402
        && code.indexOf("Uint32Array") == -1         // bug 1124421
        && code.indexOf("Float32Array") == -1        // bug 1124421
        && code.indexOf("Math.pow") == -1            // bug 1124485
-       && code.indexOf("Math.max") == -1            // bug 1126066
+       // We need to check if the following is fixed by bug 1122402.
+       //&& code.indexOf("Math.max") == -1            // bug 1126066
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/)) // doesn't stay valid utf-8 after going through python (?)
 
   };
