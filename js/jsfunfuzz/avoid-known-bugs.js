996c6380f3e432776433fa05718dbee2ecaa2d59
Gary Kwong <gary@rumblingedge.com>
2015-02-20 23:27:11 +0800

    Add bug 1135047.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -41,6 +41,7 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("Float32Array") == -1        // bug 1124421
        && code.indexOf("Math.imul") == -1           // bug 1130679
        && code.indexOf("arguments.callee") == -1    // bug 1133389
+       && code.indexOf("Math.pow") == -1            // bug 1135047
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/)) // doesn't stay valid utf-8 after going through python (?)
 
   };
