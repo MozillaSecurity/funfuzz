e8d6ee729add411057563b08490a9b4aa48b0e77
Gary Kwong <gary@rumblingedge.com>
2015-03-12 03:51:47 -0700

    Bug 1137616, bug 1141154, bug 1141329 and bug 1141379 are now fixed.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -40,7 +40,6 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("Uint32Array") == -1         // bug 1124421
        && code.indexOf("Float32Array") == -1        // bug 1124421
        && code.indexOf("arguments.callee") == -1    // bug 1133389
-       && code.indexOf("__proto__") == -1           // bug 1137616
        && code.indexOf("toString") == -1            // bug 1137624
        && code.indexOf("Math.imul") == -1           // bug 1140890
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/)) // doesn't stay valid utf-8 after going through python (?)
