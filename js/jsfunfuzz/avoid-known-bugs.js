c31c4bab342a1b0bdce7ebf872fbf4bf01c2f4db
Gary Kwong <gary@rumblingedge.com>
2015-03-31 15:10:52 -0700

    Ignore dumpHeapComplete output when running compareJIT.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -22,6 +22,7 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("timeout") == -1             // time runs and crawls
        && code.indexOf("random") == -1
        && code.indexOf("dumpObject") == -1          // shows heap addresses
+       && code.indexOf("dumpHeapComplete") == -1    // shows heap addresses
        && code.indexOf("oomAfterAllocations") == -1
        && code.indexOf("load") == -1                // load()ed regression test might output dates, etc
        && code.indexOf("drainAllocationsLog") == -1 // drainAllocationsLog returns an object with a timestamp, see bug 1066313
