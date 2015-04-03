efa03f3e709a638b745c14e66f10f136e7190604
Jesse Ruderman <jruderman@gmail.com>
2015-04-03 10:18:44 -0700

    compileAndGo was removed in https://bugzilla.mozilla.org/show_bug.cgi?id=679939

--- js/jsfunfuzz/gen-type-aware-code.js
+++ js/jsfunfuzz/gen-type-aware-code.js
@@ -164,7 +164,6 @@ var makeEvilCallback;
     return ("({ global: " + m("g") +
       ", fileName: " + Random.index(["'evaluate.js'", "null"]) +
       ", lineNumber: 42, newContext: " + makeBoolean(d, b) +
-      ", compileAndGo: " + makeBoolean(d, b) +
       ", noScriptRval: " + makeBoolean(d, b) +
       ", catchTermination: " + makeBoolean(d, b) +
       ", saveFrameChain: " + ("bug 881999" && rnd(10000) ? "false" : makeBoolean(d, b)) +
