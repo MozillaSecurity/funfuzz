ea069871657694824d5e6b803100df3d9e6b7806
Gary Kwong <gary@rumblingedge.com>
2015-04-21 14:50:33 -0700

    Stop testing --unboxed-objects until outstanding bugs are fixed.

--- js/shellFlags.py
+++ js/shellFlags.py
@@ -52,8 +52,9 @@ def randomFlagSet(shellPath):
     if shellSupportsFlag(shellPath, '--no-incremental-gc') and chance(.1):
         args.append("--no-incremental-gc")  # --no-incremental-gc landed in bug 958492
 
-    if shellSupportsFlag(shellPath, '--unboxed-objects') and chance(.2):
-        args.append("--unboxed-objects")
+    # Disabled until all outstanding --unboxed-objects bugs are fixed
+    #if shellSupportsFlag(shellPath, '--unboxed-objects') and chance(.2):
+    #    args.append("--unboxed-objects")
 
     #if shellSupportsFlag(shellPath, '--ion-sink=on') and chance(.2):
     #    args.append("--ion-sink=on")  # --ion-sink=on landed in bug 1093674
