2b4f2e515cacd199819ddfc3dff9af2b830977ab
Jesse Ruderman <jruderman@gmail.com>
2015-05-19 03:52:13 -0700

    Move forkJoin out of bot.py and into its own file

--- bot.py
+++ bot.py
@@ -25,6 +25,7 @@ import downloadBuild
 import hgCmds
 import lithOps
 import subprocesses as sps
+import forkJoin
 from LockDir import LockDir
 path2 = os.path.abspath(os.path.join(path0, 'dom', 'automation'))
 sys.path.append(path2)
@@ -378,7 +379,7 @@ def botmain(options):
                 # as x86/64 ones, so restrict fuzzing to only 1 core for now.
                 numProcesses = 1
 
-            forkJoin(options.tempDir, numProcesses, fuzzUntilBug, options, buildInfo)
+            forkJoin.forkJoin(options.tempDir, numProcesses, fuzzUntilBug, options, buildInfo)
 
     # Remove build directory if we created it
     if options.testType == 'dom' and not \
@@ -568,56 +569,6 @@ def ensureBuild(options):
     return BuildInfo(bDir, bType, bSrc, bRev, manyTimedRunArgs)
 
 
-# Call |fun| in a bunch of separate processes, then wait for them all to finish.
-# fun is called with someArgs, plus an additional argument with a numeric ID.
-# |fun| must be a top-level function (not a closure) so it can be pickled on Windows.
-def forkJoin(logDir, numProcesses, fun, *someArgs):
-    def showFile(fn):
-        print "==== %s ====" % fn
-        print
-        with open(fn) as f:
-            for line in f:
-                print line.rstrip()
-        print
-
-    # Fork a bunch of processes
-    print "Forking %d children..." % numProcesses
-    ps = []
-    for i in xrange(numProcesses):
-        p = multiprocessing.Process(target=redirectOutputAndCallFun, args=[logDir, i, fun, someArgs], name="Parallel process " + str(i))
-        p.start()
-        ps.append(p)
-
-    # Wait for them all to finish, and splat their outputs
-    for i in xrange(numProcesses):
-        p = ps[i]
-        print "=== Waiting for child #%d (%d) to finish... ===" % (i, p.pid)
-        p.join()
-        print "=== Child process #%d exited with code %d ===" % (i, p.exitcode)
-        print
-        showFile(logFileName(logDir, i, "out"))
-        showFile(logFileName(logDir, i, "err"))
-        print
-
-# Functions used by forkJoin are top-level so they can be "pickled" (required on Windows)
-def logFileName(logDir, i, t):
-    return os.path.join(logDir, "forkjoin-" + str(i) + "-" + t + ".txt")
-def redirectOutputAndCallFun(logDir, i, fun, someArgs):
-    sys.stdout = open(logFileName(logDir, i, "out"), 'w')
-    sys.stderr = open(logFileName(logDir, i, "err"), 'w')
-    fun(*(someArgs + (i,)))
-
-# You should see "Green Chairs" from the first few processes, then a pause
-# and error from process 5, then "Green Chairs" again from the rest.
-def test_forkJoin():
-    forkJoin(".", 8, test_forkJoin_inner, "Green", "Chairs")
-def test_forkJoin_inner(adj, noun, forkjoin_id):
-    import time
-    print adj + " " + noun
-    print forkjoin_id
-    if forkjoin_id == 5:
-        time.sleep(1)
-        print ({}).a # error
 
 
 def fuzzUntilBug(options, buildInfo, i):
---
+++ util/forkJoin.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import multiprocessing
+import os
+import sys
+
+
+# Call |fun| in a bunch of separate processes, then wait for them all to finish.
+# fun is called with someArgs, plus an additional argument with a numeric ID.
+# |fun| must be a top-level function (not a closure) so it can be pickled on Windows.
+def forkJoin(logDir, numProcesses, fun, *someArgs):
+    def showFile(fn):
+        print "==== %s ====" % fn
+        print
+        with open(fn) as f:
+            for line in f:
+                print line.rstrip()
+        print
+
+    # Fork a bunch of processes
+    print "Forking %d children..." % numProcesses
+    ps = []
+    for i in xrange(numProcesses):
+        p = multiprocessing.Process(target=redirectOutputAndCallFun, args=[logDir, i, fun, someArgs], name="Parallel process " + str(i))
+        p.start()
+        ps.append(p)
+
+    # Wait for them all to finish, and splat their outputs
+    for i in xrange(numProcesses):
+        p = ps[i]
+        print "=== Waiting for child #%d (%d) to finish... ===" % (i, p.pid)
+        p.join()
+        print "=== Child process #%d exited with code %d ===" % (i, p.exitcode)
+        print
+        showFile(logFileName(logDir, i, "out"))
+        showFile(logFileName(logDir, i, "err"))
+        print
+
+
+# Functions used by forkJoin are top-level so they can be "pickled" (required on Windows)
+def logFileName(logDir, i, t):
+    return os.path.join(logDir, "forkjoin-" + str(i) + "-" + t + ".txt")
+
+
+def redirectOutputAndCallFun(logDir, i, fun, someArgs):
+    sys.stdout = open(logFileName(logDir, i, "out"), 'w')
+    sys.stderr = open(logFileName(logDir, i, "err"), 'w')
+    fun(*(someArgs + (i,)))
+
+
+# You should see:
+# * "Green Chairs" from the first few processes
+# * A pause and error (with stack trace) from process 5
+# * "Green Chairs" again from the rest.
+def test_forkJoin():
+    forkJoin(".", 8, test_forkJoin_inner, "Green", "Chairs")
+
+
+def test_forkJoin_inner(adj, noun, forkjoin_id):
+    import time
+    print adj + " " + noun
+    print forkjoin_id
+    if forkjoin_id == 5:
+        time.sleep(1)
+        ERROR
+
+
+if __name__ == "__main__":
+    print "test_forkJoin():"
+    test_forkJoin()
