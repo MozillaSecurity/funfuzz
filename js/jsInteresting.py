a534513398f204f71048a67d3ff48a0fe16d0c98
Jesse Ruderman <jruderman@gmail.com>
2015-06-16 14:41:48 -0700

    Whitespace

--- js/jsInteresting.py
+++ js/jsInteresting.py
@@ -27,25 +27,42 @@ import subprocesses as sps
 # Fuzzing will note the level, and pass it to Lithium.
 # Lithium is allowed to go to a higher level.
 JS_LEVELS = 10
-JS_LEVEL_NAMES = ["fine", "known crash", "timed out", "abnormal", "jsfunfuzz did not finish", "jsfunfuzz decided to exit", "overall mismatch", "valgrind error", "malloc error", "new assert or crash"]
+JS_LEVEL_NAMES = [
+    "fine",
+    "known crash",
+    "timed out",
+    "abnormal",
+    "jsfunfuzz did not finish",
+    "jsfunfuzz decided to exit",
+    "overall mismatch",
+    "valgrind error",
+    "malloc error",
+    "new assert or crash"
+]
 assert len(JS_LEVEL_NAMES) == JS_LEVELS
-(  JS_FINE,
-   JS_KNOWN_CRASH, JS_TIMED_OUT,                          # frustrates understanding of stdout; not even worth reducing
-   JS_ABNORMAL_EXIT,                                      # frustrates understanding of stdout; can mean several things
-   JS_DID_NOT_FINISH, JS_DECIDED_TO_EXIT,                 # specific to jsfunfuzz
-   JS_OVERALL_MISMATCH,                                   # specific to comparejit (set in compareJIT.py)
-   JS_VG_AMISS, JS_MALLOC_ERROR, JS_NEW_ASSERT_OR_CRASH   # stuff really going wrong
+(
+    JS_FINE,
+    JS_KNOWN_CRASH,                     # frustrates understanding of stdout; not even worth reducing
+    JS_TIMED_OUT,                       # frustrates understanding of stdout; not even worth reducing
+    JS_ABNORMAL_EXIT,                   # frustrates understanding of stdout; can mean several things
+    JS_DID_NOT_FINISH,                  # correctness (only jsfunfuzzLevel)
+    JS_DECIDED_TO_EXIT,                 # correctness (only jsfunfuzzLevel)
+    JS_OVERALL_MISMATCH,                # correctness (only compareJIT)
+    JS_VG_AMISS,                        # memory safety
+    JS_MALLOC_ERROR,                    # memory safety
+    JS_NEW_ASSERT_OR_CRASH              # memory safety or other issue that is definitely a bug
 ) = range(JS_LEVELS)
 
 
 VALGRIND_ERROR_EXIT_CODE = 77
+
 
 def baseLevel(runthis, timeout, knownPath, logPrefix, valgrind=False):
     if valgrind:
         runthis = (
             inspectShell.constructVgCmdList(errorCode=VALGRIND_ERROR_EXIT_CODE) +
             valgrindSuppressions(knownPath) +
-          runthis)
+            runthis)
 
     preexec_fn = ulimitSet if os.name == 'posix' else None
     runinfo = timedRun.timed_run(runthis, timeout, logPrefix, preexec_fn=preexec_fn)
@@ -165,10 +182,10 @@ def jsfunfuzzLevel(options, logPrefix, quiet=False):
             statusIssueList.append('Status: ' + i)
         assert len(statusIssueList) != 0
         fileManipulation.writeLinesToFile(
-          ['Number: ' + logPrefix + '\n',
-           'Command: ' + sps.shellify(options.jsengineWithArgs) + '\n'] +
-          [i + '\n' for i in statusIssueList],
-          logPrefix + '-summary.txt')
+            ['Number: ' + logPrefix + '\n',
+             'Command: ' + sps.shellify(options.jsengineWithArgs) + '\n'] +
+            [i + '\n' for i in statusIssueList],
+            logPrefix + '-summary.txt')
 
     if not quiet:
         print logPrefix + " | " + summaryString(issues, lev, runinfo.elapsedtime)
@@ -193,6 +210,7 @@ def hitMemoryLimit(err):
 
     return None
 
+
 def oomed(logPrefix):
     # spidermonkey shells compiled with --enable-more-deterministic will tell us on stderr if they run out of memory
     with open(logPrefix + "-err.txt", "rb") as f:
@@ -206,13 +224,16 @@ def summaryString(issues, level, elapsedtime):
     amissDetails = ("") if (len(issues) == 0) else (" | " + repr(issues[:5]) + " ")
     return "%5.1fs | %d | %s%s" % (elapsedtime, level, JS_LEVEL_NAMES[level], amissDetails)
 
+
 def truncateFile(fn, maxSize):
     if os.path.exists(fn) and os.path.getsize(fn) > maxSize:
         with open(fn, "r+") as f:
             f.truncate(maxSize)
 
+
 def valgrindSuppressions(knownPath):
     return ["--suppressions=" + filename for filename in findIgnoreLists.findIgnoreLists(knownPath, "valgrind.txt")]
+
 
 def deleteLogs(logPrefix):
     """Whoever calls baseLevel should eventually call deleteLogs (unless a bug was found)."""
@@ -220,12 +241,12 @@ def deleteLogs(logPrefix):
     # the search indexer, anti-virus realtime protection and backup applications.
     os.remove(logPrefix + "-out.txt")
     os.remove(logPrefix + "-err.txt")
-    if (os.path.exists(logPrefix + "-crash.txt")):
+    if os.path.exists(logPrefix + "-crash.txt"):
         os.remove(logPrefix + "-crash.txt")
-    if (os.path.exists(logPrefix + "-vg.xml")):
+    if os.path.exists(logPrefix + "-vg.xml"):
         os.remove(logPrefix + "-vg.xml")
     # FIXME: in some cases, subprocesses.py gzips a core file only for us to delete it immediately.
-    if (os.path.exists(logPrefix + "-core.gz")):
+    if os.path.exists(logPrefix + "-core.gz"):
         os.remove(logPrefix + "-core.gz")
 
 
@@ -249,17 +270,17 @@ def parseOptions(args):
     parser = OptionParser()
     parser.disable_interspersed_args()
     parser.add_option("--valgrind",
-                      action = "store_true", dest = "valgrind",
-                      default = False,
-                      help = "use valgrind with a reasonable set of options")
+                      action="store_true", dest="valgrind",
+                      default=False,
+                      help="use valgrind with a reasonable set of options")
     parser.add_option("--minlevel",
-                      type = "int", dest = "minimumInterestingLevel",
-                      default = JS_FINE + 1,
-                      help = "minimum js/jsInteresting.py level for lithium to consider the testcase interesting")
+                      type="int", dest="minimumInterestingLevel",
+                      default=JS_FINE + 1,
+                      help="minimum js/jsInteresting.py level for lithium to consider the testcase interesting")
     parser.add_option("--timeout",
-                      type = "int", dest = "timeout",
-                      default = 120,
-                      help = "timeout in seconds")
+                      type="int", dest="timeout",
+                      default=120,
+                      help="timeout in seconds")
     options, args = parser.parse_args(args)
     if len(args) < 2:
         raise Exception("Not enough positional arguments")
@@ -282,6 +303,7 @@ def interesting(args, tempPrefix):
     truncateFile(tempPrefix + "-out.txt", 1000000)
     truncateFile(tempPrefix + "-err.txt", 1000000)
     return actualLevel >= gOptions.minimumInterestingLevel
+
 
 if __name__ == "__main__":
     options = parseOptions(sys.argv[1:])
