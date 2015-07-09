40f7a45e63c39a2e897063c3a02a8b960b5c9dc0
Gary Kwong <gary@rumblingedge.com>
2015-07-08 18:22:29 -0700

    Fix #3 - First stab at making reposUpdate work with both Git and Mercurial repositories.

--- util/reposUpdate.py
+++ util/reposUpdate.py
@@ -20,11 +20,11 @@ logger = logging.getLogger(__name__)
 ESR_NOW = 31
 ESR_NEXT = ESR_NOW + 7
 
-path0 = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
-path1 = os.path.abspath(os.path.join(path0, os.pardir))
+THIS_SCRIPT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+REPO_PARENT_PATH = os.path.abspath(os.path.join(THIS_SCRIPT_DIRECTORY, os.pardir, os.pardir))
 
 # Add your repository here. Note that Valgrind does not have a hg repository.
-REPOS = ['fuzzing'] + ['mozilla-' + x for x in [
+REPOS = ['gecko-dev', 'lithium'] + ['mozilla-' + x for x in [
             'inbound', 'central', 'aurora', 'beta', 'release',
             'esr' + str(ESR_NOW), 'esr' + str(ESR_NEXT)
     ]
@@ -34,7 +34,7 @@ def typeOfRepo(r):
     '''Returns the type of repository.'''
     repoList = []
     repoList.append('.hg')
-    #repoList.append('.git')
+    repoList.append('.git')
     for rtype in repoList:
         if os.path.isdir(os.path.join(r, rtype)):
             return rtype[1:]
@@ -43,50 +43,42 @@ def typeOfRepo(r):
 
 def updateRepo(repo):
     '''Updates a repository. Returns False if missing; returns True if successful; raises an exception if updating fails.'''
+    assert os.path.isdir(repo)
+    repoType = typeOfRepo(repo)
 
-    # Find the repo, or return False if we can't find it
-    locs = [
-        os.path.normpath(os.path.join(path1, repo)),
-        os.path.normpath(os.path.join(path1, 'trees', repo))
-    ]
-    for loc in locs:
-        if os.path.isdir(loc):
-            repoLocation = loc
-            break
-    else:
-        logger.info("We didn't find a repo in any of: %s\n" % repr(locs))
-        return False
-
-    logger.info('Found %s repository in %s' % (repo, repoLocation))
-    repoType = typeOfRepo(repoLocation)
-
-    # Update the repo, or sys.exit(1) if we can't update it
     if repoType == 'hg':
         _, retval = sps.timeSubprocess(['hg', 'pull', '-u'],
-            ignoreStderr=True, combineStderr=True, cwd=repoLocation, vb=True)
+            ignoreStderr=True, combineStderr=True, cwd=repo, vb=True)
         sps.timeSubprocess(['hg', 'log', '-r', 'default'],
-            cwd=repoLocation, vb=True)
-    #elif repoType == 'git':
-        # This needs to be looked at. When ready, re-enable in typeOfRepo function.
+            cwd=repo, vb=True)
+    elif repoType == 'git':
         # Ignore exit codes so the loop can continue retrying up to number of counts.
-        # gitStdout, retval = sps.timeSubprocess(
-        #     ['git', 'fetch'], ignoreStderr=True, combineStderr=True, ignoreExitCode=True,
-        #     cwd=repoLocation, vb=True)
-        # gitStdout, retval = sps.timeSubprocess(
-        #     ['git', 'checkout'], ignoreStderr=True, combineStderr=True, ignoreExitCode=True,
-        #     cwd=repoLocation, vb=True)
+        sps.timeSubprocess(['git', 'fetch'],
+                           ignoreStderr=True, combineStderr=True, ignoreExitCode=True, cwd=repo, vb=True)
+        sps.timeSubprocess(['git', 'merge', '--ff-only', 'origin/master'],
+                           ignoreStderr=True, combineStderr=True, ignoreExitCode=True, cwd=repo, vb=True)
     else:
         raise Exception('Unknown repository type: ' + repoType)
 
     return True
 
 
+def updateRepos():
+    '''Updates Mercurial and Git repositories located in ~ and ~/trees .'''
+    trees = [
+        os.path.normpath(os.path.join(REPO_PARENT_PATH)),
+        os.path.normpath(os.path.join(REPO_PARENT_PATH, 'trees'))
+    ]
+    for tree in trees:
+        for name in sorted(os.listdir(tree)):
+            if name in REPOS or name.startswith("funfuzz"):
+                print 'Updating %s ...' % name
+                updateRepo(os.path.join(tree, name))
+
+
 def main():
     logger.info(sps.dateStr())
-
-    for repo in REPOS:
-        updateRepo(repo)
-
+    updateRepos()
     logger.info(sps.dateStr())
 
 
