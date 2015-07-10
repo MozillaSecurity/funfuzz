740be12560269de6db820893dd5500f3f82bed85
Gary Kwong <gary@rumblingedge.com>
2015-07-09 18:19:25 -0700

    Bug 1180608 and bug 1181354 are now fixed.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -46,7 +46,6 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("Object.seal") == -1             // bug 937922
        && code.indexOf("length") == -1                  // bug 1027846
        && code.indexOf("preventExtensions") == -1       // bug 1085299
-       && code.indexOf("Uint8ClampedArray") == -1       // bug 1181354
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/))  // doesn't stay valid utf-8 after going through python (?)
 
   };
