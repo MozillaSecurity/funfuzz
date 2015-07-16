e27045bbbd619d20d559d905ceacae66e8cfadc2
Gary Kwong <gary@rumblingedge.com>
2015-07-16 16:58:09 -0700

    Tweak MozillaBuild script architecture support to make it work with MozillaBuild 2.0.0.

--- util/subprocesses.py
+++ util/subprocesses.py
@@ -25,8 +25,7 @@ isWin64 = ('PROGRAMFILES(X86)' in os.environ)
 isWinVistaOrHigher = isWin and (sys.getwindowsversion()[0] >= 6)
 # This refers to the Win-specific "MozillaBuild" environment in which Python is running, which is
 # spawned from the MozillaBuild script for 64-bit compilers, e.g. start-msvc10-x64.bat
-isMozBuild64 = (os.name == 'nt') and ('x64' in os.environ['MOZ_TOOLS'].split(os.sep)[-1])
-# isMozBuild64 = isWin and '64' in os.environ['MOZ_MSVCBITS']  # For MozillaBuild 2.0.0
+isMozBuild64 = isWin and '64' in os.environ['MOZ_MSVCBITS']  # For MozillaBuild 2.0.0
 
 noMinidumpMsg = r'''
 WARNING: Minidumps are not being generated, so all crashes will be uninteresting.
