d253004cdeb8996a6535d6436d1f3ee64050488a
Jesse Ruderman <jruderman@gmail.com>
2015-07-16 19:12:51 -0700

    Ignore bug 1184807

--- dom/automation/domInteresting.py
+++ dom/automation/domInteresting.py
@@ -190,6 +190,9 @@ class AmissLogHandler:
             if "Did not receive all required callbacks" in msg:
                 print "Ignoring memory leaks (bug 973384)"
                 self.expectedToLeak = True
+            if "leaking" in msg:
+                print "Ignoring memory leaks"
+                self.expectedToLeak = True
             if self.nsassertionCount == 100:
                 print "domInteresting.py: not considering it a failure if browser hangs, because assertions are slow with stack-printing on. Please test in opt builds too, or fix the assertion bugs."
                 self.expectedToHang = True
