e951735716441756d319c8f81bbfa810b3fa0a11
Gary Kwong <gary@rumblingedge.com>
2015-08-20 13:14:03 -0700

    Add support for --ion-shared-stubs=on.

--- autobisect-js/knownBrokenEarliestWorking.py
+++ autobisect-js/knownBrokenEarliestWorking.py
@@ -109,6 +109,8 @@ def earliestKnownWorkingRev(options, flags, skipRevs):
 
     required = []
 
+    if "--ion-shared-stubs=on" in flags:
+        required.append('3655d19ce241')  # m-c 257573 Fx43, 1st w/--ion-shared-stubs=on, see bug 1168756
     if options.enableSimulatorArm64:
         # This should get updated whenever ARM64 builds are stable, probably ~end-June 2015
         required.append('25e99bc12482')  # m-c 249239 Fx41, 1st w/--enable-simulator=[arm|arm64|mips], see bug 1173992
--- js/shellFlags.py
+++ js/shellFlags.py
@@ -43,6 +43,9 @@ def randomFlagSet(shellPath):
     if shellSupportsFlag(shellPath, '--fuzzing-safe'):
         args.append("--fuzzing-safe")  # --fuzzing-safe landed in bug 885361
 
+    if shellSupportsFlag(shellPath, '--ion-shared-stubs=on') and chance(.2):
+        args.append("--ion-shared-stubs=on")  # --ion-shared-stubs=on landed in bug 1168756
+
     if shellSupportsFlag(shellPath, '--non-writable-jitcode') and chance(.3):
         args.append("--non-writable-jitcode")  # --non-writable-jitcode landed in bug 977805
 
