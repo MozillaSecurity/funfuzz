958d16a09e8c3feef4b0dbdc3b2059a64faf60b4
Gary Kwong <gary@rumblingedge.com>
2015-09-11 00:34:28 -0700

    Add bug 1203862.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -46,6 +46,7 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("Object.seal") == -1             // bug 937922
        && code.indexOf("length") == -1                  // bug 1027846
        && code.indexOf("preventExtensions") == -1       // bug 1085299
+       && code.indexOf("new") == -1                     // bug 1203862
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/))  // doesn't stay valid utf-8 after going through python (?)
 
   };
