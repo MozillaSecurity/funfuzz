6705d167c86cd053ca1776a7311a7d0a65ed7b77
Gary Kwong <gary@rumblingedge.com>
2015-09-28 10:25:52 -0700

    Bug 1207449 is now fixed.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -47,7 +47,6 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("length") == -1                  // bug 1027846
        && code.indexOf("preventExtensions") == -1       // bug 1085299
        && code.indexOf("new") == -1                     // bug 1203862
-       && code.indexOf("Math.fround") == -1             // bug 1207449
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/))  // doesn't stay valid utf-8 after going through python (?)
 
   };
