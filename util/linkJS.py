8541ec09e237f1401095d31177bdde9ac1adaa39
Jesse Ruderman <jruderman@gmail.com>
2015-11-03 20:35:36 -0800

    Include full path to original files

--- util/linkJS.py
+++ util/linkJS.py
@@ -12,17 +12,17 @@ def linkJS(target_fn, file_list_fn, source_base, prologue="", module_dirs=[]):
             for source_fn in file_list:
                 source_fn = source_fn.replace("/", os.path.sep).strip()
                 if len(source_fn) > 0 and source_fn[0] != "#":
-                    addContents(source_base, source_fn, target)
+                    addContents(os.path.join(source_base, source_fn), target)
 
         # Add all *.js files in module_dirs
         for module_base in module_dirs:
             for module_fn in os.listdir(module_base):
                 if module_fn.endswith(".js"):
-                    addContents(module_base, module_fn, target)
+                    addContents(os.path.join(module_base, module_fn), target)
 
 
-def addContents(source_base, source_fn, target):
+def addContents(source_fn, target):
     target.write("\n\n// " + source_fn + "\n\n")
-    with open(os.path.join(source_base, source_fn)) as source:
+    with open(source_fn) as source:
         for line in source:
             target.write(line)
