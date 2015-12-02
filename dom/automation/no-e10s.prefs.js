9353c24b3a8222638766d1beb8e095f8428e911f
Jesse Ruderman <jruderman@gmail.com>
2015-12-02 14:59:06 -0800

    Add stuff to aid retesting DOM testcases

---
+++ dom/automation/no-e10s.prefs.js
@@ -0,0 +1,3 @@
+user_pref("browser.tabs.remote.autostart", false);
+user_pref("browser.tabs.remote.autostart.1", false);
+user_pref("browser.tabs.remote.autostart.2", false);
---
+++ dom/automation/retestLocalW.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import re
+import subprocess
+import sys
+import time
+
+build = sys.argv[1]
+fullu = os.path.expanduser("~")
+dominteresting_py = os.path.join(fullu, "funfuzz", "dom", "automation", "domInteresting.py")
+
+
+def timestamp():
+    return str(int(time.time()))
+
+
+for wn in os.listdir(fullu):
+    if wn.startswith("wtmp"):
+        fullwn = os.path.join(fullu, wn)
+        for filename in os.listdir(fullwn):
+            match = re.match(r"(q\d+)-splice-reduced\..*", filename)
+            if match:
+                fullFilename = os.path.join(fullwn, filename)
+                dicall = [sys.executable, dominteresting_py, "--background", build, fullFilename]
+                outfn = os.path.join(fullwn, match.group(1) + "-retest-out-" + timestamp() + ".txt")
+                with open(outfn, "w") as out:
+                    if subprocess.call(dicall, stdout=out, stderr=subprocess.STDOUT):
+                        print fullFilename
+                        print outfn
+
+
+# pbpaste | xargs open -a "Sublime Text"
