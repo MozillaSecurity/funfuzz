a65c2b589b902cd838bfbc3495282428d18ce2bd
Gary Kwong <gary@rumblingedge.com>
2016-02-08 12:51:52 +0800

    Add bug 1246552.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -57,6 +57,7 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("localeCompare") == -1           // bug 1244502
        && code.indexOf(".filter") == -1                 // bug 1245187
        && code.indexOf("Number.MAX_VALUE") == -1        // bug 1246200
+       && code.indexOf(".sort") == -1                   // bug 1246552
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/))  // doesn't stay valid utf-8 after going through python (?)
 
   };
