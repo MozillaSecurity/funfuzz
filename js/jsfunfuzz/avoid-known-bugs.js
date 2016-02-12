45035b4d0fb2f5bb709cefab55196abd86f8c268
Gary Kwong <gary@rumblingedge.com>
2016-02-12 18:44:28 +0800

    Bug 1244502 seems to involve .call instead of localeCompare.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -55,7 +55,7 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("length") == -1                  // bug 1027846
        && code.indexOf("preventExtensions") == -1       // bug 1085299
        && code.indexOf("Math.round") == -1              // bug 1236114
-       && code.indexOf("localeCompare") == -1           // bug 1244502
+       && code.indexOf(".call") == -1                   // bug 1244502
        && code.indexOf("with") == -1                    // bug 1245187
        && code.indexOf("Number.MAX_VALUE") == -1        // bug 1246200
        && code.indexOf(".sup") == -1                    // bug 1246552
