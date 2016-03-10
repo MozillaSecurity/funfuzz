5bc858c97d18be0b159468e136b2323be27dbdb5
Gary Kwong <gary@rumblingedge.com>
2016-03-09 18:50:36 -0800

    Let's just create the ~/sigcache directory if it doesn't exist when createCollector is called.

--- util/createCollector.py
+++ util/createCollector.py
@@ -14,12 +14,12 @@ from Collector.Collector import Collector
 
 def createCollector(tool):
     assert tool == "DOMFuzz" or tool == "jsfunfuzz"
-    sCacheDir = None
     cacheDir = os.path.normpath(os.path.expanduser(os.path.join("~", "sigcache")))
-    if os.path.isdir(cacheDir):
-        sCacheDir = cacheDir
-    # Define sigCacheDir on instantiation else jsInteresting when run alone will complain
-    collector = Collector(sigCacheDir=sCacheDir, tool=tool)
+    try:
+        os.mkdir(cacheDir)
+    except OSError:
+        pass  # cacheDir already exists
+    collector = Collector(sigCacheDir=cacheDir, tool=tool)
     return collector
 
 
