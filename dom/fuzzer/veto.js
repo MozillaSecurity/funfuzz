15bfda2ae67762a0b405de7fbc945055056f9501
Jesse Ruderman <jruderman@gmail.com>
2016-03-22 23:28:17 -0700

    Ignore bug 1258076

--- dom/fuzzer/veto.js
+++ dom/fuzzer/veto.js
@@ -222,6 +222,7 @@ function fuzzInitBlacklists()
                 "transformStyle",   // bug 767233
                 "position",         // bug 1036750
                 "caption",          // bug 1156244
+                "maction",          // bug 1258076
 
                 // Mutation events might be triggered by the test itself
                 "DOMSubtreeModified",
