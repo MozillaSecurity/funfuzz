657128f593a592a2fba4e0b013cf116f790fbc5b
Gary Kwong <gary@rumblingedge.com>
2016-04-26 01:18:06 -0400

    Bug 1263811 is now fixed.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -57,7 +57,6 @@ function whatToTestSpidermonkeyTrunk(code)
        && code.indexOf("disassemble") == -1             // see bug 1237403 (related to asm.js)
        && code.indexOf("Number.MAX_VALUE") == -1        // bug 1246200
        && code.indexOf("prototype") == -1               // bug 1263525
-       && code.indexOf("arguments") == -1               // bug 1263811
        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/))  // doesn't stay valid utf-8 after going through python (?)
 
   };
