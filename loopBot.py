16e729607fe3baf2c62203624efcc709536f8cae
Jesse Ruderman <jruderman@gmail.com>
2016-05-03 12:35:54 -0700

    loopBot.py: cleanup

--- loopBot.py
+++ loopBot.py
@@ -7,24 +7,28 @@
 
 import os
 import sys
-import platform
 import subprocess
 import time
 
 path0 = os.path.dirname(os.path.abspath(__file__))
 
-# This junk should be moved to bot.py, OR moved into a config file, OR this file should subprocess-call ITSELF rather than using a while loop.
+# Config-ish bits should move to bot.py, OR move into a config file, OR this file should subprocess-call ITSELF rather than using a while loop.
+
+
 def buildOptionsASan():
     mozconfig = os.path.expanduser("~/funfuzz/dom/mozconfig/mozconfig-asan")
     srcdir = os.path.expanduser("~/trees/mozilla-central/")
     objdir = srcdir + "obj-firefox-asan/"
     return " ".join(["--mozconfig", mozconfig, "--repoDir", srcdir, "--objDir", objdir])
+
+
 def botArgs():
     if sys.argv[1:] == ['--dom-defaults']:
         return ["--test-type=dom", "--target-time=43200"]
     if sys.argv[1:] == ['--dom-asan']:
         return ["--test-type=dom", "--target-time=43200", "--build-options", buildOptionsASan()]
     return sys.argv[1:]
+
 
 def loopSequence(cmdSequence, waitTime):
     """Call a sequence of commands in a loop. If any fails, sleep(waitTime) and go back to the beginning of the sequence."""
@@ -37,11 +41,13 @@ def loopSequence(cmdSequence, waitTime):
                 subprocess.check_call(cmd)
             except subprocess.CalledProcessError as e:
                 print "Something went wrong when calling: " + repr(cmd)
+                print repr(e)
                 import traceback
-                print(traceback.format_exc())
+                print traceback.format_exc()
                 print "Waiting %d seconds..." % waitTime
                 time.sleep(waitTime)
                 break
+
 
 def main():
     loopSequence([
