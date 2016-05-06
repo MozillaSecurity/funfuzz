9a2407ea783904242465f460635af10bde145af9
Gary Kwong <gary@rumblingedge.com>
2016-05-06 14:02:00 -0700

    Disable oomTest on ESR45 for now as ESR45 is still noisy with OOM bugs.

--- js/jsfunfuzz/gen-grammar.js
+++ js/jsfunfuzz/gen-grammar.js
@@ -271,7 +271,7 @@ var statementMakers = Random.weighted([
   //{ w: 3, v: function(d, b) { return "var opn = Object.getOwnPropertyNames(" + makeId(d, b) + "); for (var j = 0; j < opn.length; ++j) { addPropertyName(opn[j]); }"; } },
 ]);
 
-if (typeof oomTest == "function") {
+if (typeof oomTest == "function" && engine != ENGINE_SPIDERMONKEY_MOZILLA45) {
   statementMakers = statementMakers.concat([
     function(d, b) { return "oomTest(" + makeFunction(d, b-1) + ")"; },
   ]);
@@ -1314,7 +1314,7 @@ if (typeof XPCNativeWrapper == "function") {
   ]);
 }
 
-if (typeof oomTest == "function") {
+if (typeof oomTest == "function" && engine != ENGINE_SPIDERMONKEY_MOZILLA45) {
   functionMakers = functionMakers.concat([
     function(d, b) { return "oomTest"; }
   ]);
