8f314ad8007c09d38008d2a0faa04c4dc93104d3
Gary Kwong <gary@rumblingedge.com>
2017-02-28 17:51:41 -0800

    Newer Homebrew installs have autoconf213 in a separate location.

--- js/compileShell.py
+++ js/compileShell.py
@@ -184,6 +184,9 @@ def autoconfRun(cwDir):
     if sps.isMac:
         autoconf213MacBin = '/usr/local/Cellar/autoconf213/2.13/bin/autoconf213' \
                             if sps.isProgramInstalled('brew') else 'autoconf213'
+        # Total hack to support new and old Homebrew configs, we can probably just call autoconf213
+        if not sps.normExpUserPath(os.path.isfile(autoconf213MacBin)):
+            autoconf213MacBin = 'autoconf213'
         subprocess.check_call([autoconf213MacBin], cwd=cwDir)
     elif sps.isLinux:
         # FIXME: We should use a method that is similar to the client.mk one, as per
@@ -271,6 +274,9 @@ def cfgBin(shell):
             cfgEnvDt['CROSS_COMPILE'] = '1'
             if sps.isProgramInstalled('brew'):
                 cfgEnvDt['AUTOCONF'] = '/usr/local/Cellar/autoconf213/2.13/bin/autoconf213'
+                # Hacked up for new and old Homebrew configs, we can probably just call autoconf213
+                if not sps.normExpUserPath(os.path.isfile(cfgEnvDt['AUTOCONF'])):
+                    cfgEnvDt['AUTOCONF'] = 'autoconf213'
             cfgCmdList.append('sh')
             cfgCmdList.append(os.path.normpath(shell.getJsCfgPath()))
             cfgCmdList.append('--target=i386-apple-darwin14.5.0')  # Yosemite 10.10.5
