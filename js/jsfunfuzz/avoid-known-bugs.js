18e466bdb46dbc5e71670f03d28bfb16d10b69eb
Gary Kwong <gary@rumblingedge.com>
2017-05-12 15:37:04 -0700

    Remove code in jsfunfuzz specific to mozilla-esr45.

--- js/jsfunfuzz/avoid-known-bugs.js
+++ js/jsfunfuzz/avoid-known-bugs.js
@@ -66,76 +66,6 @@ function whatToTestSpidermonkeyTrunk(code)
   };
 }
 
-function whatToTestSpidermonkeyMozilla45(code)
-{
-  /* jshint laxcomma: true */
-  // regexps can't match across lines, so replace whitespace with spaces.
-  var codeL = code.replace(/\s/g, " ");
-
-  return {
-
-    allowParse: true,
-
-    allowExec: unlikelyToHang(code)
-      && (jsshell || code.indexOf("nogeckoex") == -1)
-    ,
-
-    allowIter: true,
-
-    // Ideally we'd detect whether the shell was compiled with --enable-more-deterministic
-    // Ignore both within-process & across-process, e.g. nestTest mismatch & compareJIT
-    expectConsistentOutput: true
-       && (gcIsQuiet || code.indexOf("gc") == -1)
-       && code.indexOf("/*NODIFF*/") == -1                // Ignore diff testing on these labels
-       && code.indexOf(".script") == -1                   // Debugger; see bug 1237464
-       && code.indexOf(".parameterNames") == -1           // Debugger; see bug 1237464
-       && code.indexOf(".environment") == -1              // Debugger; see bug 1237464
-       && code.indexOf(".onNewGlobalObject") == -1        // Debugger; see bug 1238246
-       && code.indexOf(".takeCensus") == -1               // Debugger; see bug 1247863
-       && code.indexOf(".findScripts") == -1              // Debugger; see bug 1250863
-       && code.indexOf("Date") == -1                      // time marches on
-       && code.indexOf("backtrace") == -1                 // shows memory addresses
-       && code.indexOf("drainAllocationsLog") == -1       // drainAllocationsLog returns an object with a timestamp, see bug 1066313
-       && code.indexOf("dumpObject") == -1                // shows heap addresses
-       && code.indexOf("dumpHeap") == -1                  // shows heap addresses
-       && code.indexOf("dumpStringRepresentation") == -1  // shows memory addresses
-       && code.indexOf("evalInWorker") == -1              // causes diffs in --no-threads vs --ion-offthread-compile=off
-       && code.indexOf("getBacktrace") == -1              // getBacktrace returns memory addresses which differs depending on flags
-       && code.indexOf("getLcovInfo") == -1
-       && code.indexOf("load") == -1                      // load()ed regression test might output dates, etc
-       && code.indexOf("offThreadCompileScript") == -1    // causes diffs in --no-threads vs --ion-offthread-compile=off
-       && code.indexOf("oomAfterAllocations") == -1
-       && code.indexOf("oomAtAllocation") == -1
-       && code.indexOf("printProfilerEvents") == -1       // causes diffs in --ion-eager vs --baseline-eager
-       && code.indexOf("validategc") == -1
-       && code.indexOf("inIon") == -1                     // may become true after several iterations, or return a string with --no-ion
-       && code.indexOf("inJit") == -1                     // may become true after several iterations, or return a string with --no-baseline
-       && code.indexOf("random") == -1
-       && code.indexOf("timeout") == -1                   // time runs and crawls
-    ,
-
-    expectConsistentOutputAcrossIter: true
-    // within-process, e.g. ignore the following items for nestTest mismatch
-       && code.indexOf("options") == -1             // options() is per-cx, and the js shell doesn't create a new cx for each sandbox/compartment
-    ,
-
-    expectConsistentOutputAcrossJITs: true
-    // across-process (e.g. running js shell with different run-time options) e.g. compareJIT
-        && code.indexOf("'strict") == -1                 // see bug 743425
-        && code.indexOf("disassemble") == -1             // see bug 1237403 (related to asm.js)
-        && code.indexOf(".length") == -1                 // bug 1027846
-        && code.indexOf("preventExtensions") == -1       // bug 1085299
-        && code.indexOf("Math.round") == -1              // see bug 1236114 - ESR45 only
-        && code.indexOf("with") == -1                    // see bug 1245187 - ESR45 only
-        && code.indexOf("Number.MAX_VALUE") == -1        // see bug 1246200 - ESR45 only
-        && code.indexOf("bailAfter") == -1               // see bug 1256324 - ESR45 only, bailAfter does not exist in ESR45
-        && code.indexOf("arguments") == -1               // see bug 1263811 - ESR45 only
-        && code.indexOf("sourceIsLazy") == -1            // see bug 1286407
-        && !( codeL.match(/\/.*[\u0000\u0080-\uffff]/))  // doesn't stay valid utf-8 after going through python (?)
-
-  };
-}
-
 function whatToTestJavaScriptCore(code)
 {
   return {
@@ -165,8 +95,6 @@ function whatToTestGeneric(code)
 var whatToTest;
 if (engine == ENGINE_SPIDERMONKEY_TRUNK)
   whatToTest = whatToTestSpidermonkeyTrunk;
-else if (engine == ENGINE_SPIDERMONKEY_MOZILLA45)
-  whatToTest = whatToTestSpidermonkeyMozilla45;
 else if (engine == ENGINE_JAVASCRIPTCORE)
   whatToTest = whatToTestJavaScriptCore;
 else
