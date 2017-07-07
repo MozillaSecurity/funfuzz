801885b720ac6335d979d898d66662a27b00ef87
Jesse Schwartzentruber <jschwartzentruber@mozilla.com>
2017-07-07 09:49:58 -0400

    Exceptions need to be instantiated.

--- util/forkJoin.py
+++ util/forkJoin.py
@@ -64,7 +64,7 @@ def test_forkJoin_inner(adj, noun, forkjoin_id):
     print forkjoin_id
     if forkjoin_id == 5:
         time.sleep(1)
-        raise NameError
+        raise NameError()
 
 
 if __name__ == "__main__":
