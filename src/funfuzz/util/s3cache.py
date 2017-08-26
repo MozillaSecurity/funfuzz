2fd37253a096690a5aadfc73e4067e6137e8fcad
Gary Kwong <gary@rumblingedge.com>
2017-08-25 18:00:31 -0700

    Fix pylint issues in "s3cache" and move some to be line-specific.

--- src/funfuzz/util/s3cache.py
+++ src/funfuzz/util/s3cache.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding=utf-8
-# pylint: disable=invalid-name,missing-docstring,missing-param-doc,missing-return-doc,missing-return-type-doc
-# pylint: disable=missing-type-doc
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+"""Functions here interact with Amazon EC2 using boto.
+"""
 
 from __future__ import absolute_import, print_function
 
@@ -14,19 +15,19 @@ import shutil
 
 from . import subprocesses as sps
 
-isBoto = False
+isBoto = False  # pylint: disable=invalid-name
 # We need to first install boto into MozillaBuild via psbootstrap on Windows
 if not sps.isMac:
     try:
         from boto.s3.connection import S3Connection, Key
         import boto.exception
         import boto.utils  # Cannot find this if only boto is imported
-        isBoto = True
+        isBoto = True  # pylint: disable=invalid-name
     except ImportError:
-        isBoto = False
+        isBoto = False  # pylint: disable=invalid-name
 
 
-def isEC2VM():
+def isEC2VM():  # pylint: disable=invalid-name,missing-return-doc,missing-return-type-doc
     """Test to see if the specified S3 cache is available."""
     if sps.isMac or not isBoto:
         return False
@@ -37,17 +38,17 @@ def isEC2VM():
         return False
 
 
-class S3Cache(object):
+class S3Cache(object):  # pylint: disable=missing-docstring
     def __init__(self, bucket_name):
         self.bucket = None
         self.bucket_name = bucket_name
 
-    def connect(self):
+    def connect(self):  # pylint: disable=missing-return-doc,missing-return-type-doc
         """Connect to the S3 bucket."""
         if not isBoto:
             return False
 
-        EC2_PROFILE = None if isEC2VM() else 'laniakea'
+        EC2_PROFILE = None if isEC2VM() else 'laniakea'  # pylint: disable=invalid-name
         try:
             conn = S3Connection(profile_name=EC2_PROFILE)
             self.bucket = conn.get_bucket(self.bucket_name)
@@ -59,7 +60,8 @@ class S3Cache(object):
             print('Unable to connect to the following bucket "%s", please check your credentials.' % self.bucket_name)
             return False
 
-    def downloadFile(self, origin, dest):
+    def downloadFile(self, origin, dest):  # pylint: disable=invalid-name,missing-param-doc,missing-return-doc
+        # pylint: disable=missing-return-type-doc,missing-type-doc
         """Download files from S3."""
         key = self.bucket.get_key(origin)
         if key is not None:
@@ -68,15 +70,17 @@ class S3Cache(object):
             return True
         return False
 
-    def compressAndUploadDirTarball(self, directory, tarball_path):
+    def compressAndUploadDirTarball(self, directory, tarball_path):  # pylint: disable=invalid-name,missing-param-doc
+        # pylint: disable=missing-type-doc
         """Compress a directory into a bz2 tarball and upload it to S3."""
         print("Creating archive...")
         shutil.make_archive(directory, 'bztar', directory)
         self.uploadFileToS3(tarball_path)
 
-    def uploadFileToS3(self, filename):
+    def uploadFileToS3(self, filename):  # pylint: disable=invalid-name,missing-param-doc,missing-type-doc
         """Upload file to S3."""
-        destDir = ''  # Root folder of the S3 bucket
+        # Root folder of the S3 bucket
+        destDir = ''  # pylint: disable=invalid-name
         destpath = os.path.join(destDir, os.path.basename(filename))
         print("Uploading %s to Amazon S3 bucket %s" % (filename, self.bucket_name))
 
@@ -84,11 +88,12 @@ class S3Cache(object):
         k.key = destpath
         k.set_contents_from_filename(filename, reduced_redundancy=True)
 
-    def uploadStrToS3(self, destDir, filename, contents):
+    def uploadStrToS3(self, destDir, filename, contents):  # pylint: disable=invalid-name,missing-param-doc
+        # pylint: disable=missing-type-doc
         """Upload a string to an S3 file."""
         print("Uploading %s to Amazon S3 bucket %s" % (filename, self.bucket_name))
 
-        k2 = Key(self.bucket)
+        k2 = Key(self.bucket)  # pylint: disable=invalid-name
         k2.key = os.path.join(destDir, filename)
         k2.set_contents_from_string(contents, reduced_redundancy=True)
         print()  # This newline is needed to get the path of the compiled binary printed on a newline.
