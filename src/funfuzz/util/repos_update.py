2f76831ec968cfe313ed9ffcc1eea3fef7f5110d
Gary Kwong <gary@rumblingedge.com>
2017-09-17 17:58:56 -0700

    Fix pylint issues in "repos_update" and move some to be line-specific.

--- src/funfuzz/util/repos_update.py
+++ src/funfuzz/util/repos_update.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # coding=utf-8
-# pylint: disable=invalid-name,missing-docstring
-# pylint: disable=missing-param-doc,missing-raises-doc,missing-return-doc,missing-return-type-doc,missing-type-doc
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# To update specified repositories to default tip and provide a short list of latest checkins.
-# Only supports hg (Mercurial) for now.
-#
-# Assumes that the repositories are located in ../../trees/*.
+"""To update specified repositories to default tip and provide a short list of latest checkins.
+Only supports hg (Mercurial) for now.
+
+Assumes that the repositories are located in ../../trees/*.
+"""
 
 from __future__ import absolute_import, print_function
 
@@ -23,7 +22,7 @@ from . import subprocesses as sps
 
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 THIS_SCRIPT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 REPO_PARENT_PATH = os.path.abspath(os.path.join(THIS_SCRIPT_DIRECTORY, os.pardir, os.pardir))
@@ -33,6 +32,7 @@ REPOS = ['gecko-dev', 'octo'] + \
     ['mozilla-' + x for x in ['inbound', 'central', 'beta', 'release']]
 
 if sps.isWin:
+    # pylint: disable=invalid-name
     git_64bit_path = os.path.normpath(os.path.join(os.getenv('PROGRAMFILES'), 'Git', 'bin', 'git.exe'))
     git_32bit_path = os.path.normpath(os.path.join(os.getenv('PROGRAMFILES(X86)'), 'Git', 'bin', 'git.exe'))
     if os.path.isfile(git_64bit_path):
@@ -45,27 +45,29 @@ else:
     GITBINARY = 'git'
 
 
-def typeOfRepo(r):
+def typeOfRepo(r):  # pylint: disable=invalid-name,missing-param-doc,missing-raises-doc,missing-return-doc
+    # pylint: disable=missing-return-type-doc,missing-type-doc
     """Return the type of repository."""
-    repoList = []
-    repoList.append('.hg')
-    repoList.append('.git')
-    for rtype in repoList:
+    repo_types = []
+    repo_types.append('.hg')
+    repo_types.append('.git')
+    for rtype in repo_types:
         if os.path.isdir(os.path.join(r, rtype)):
             return rtype[1:]
     raise Exception('Type of repository located at ' + r + ' cannot be determined.')
 
 
-def updateRepo(repo):
+def updateRepo(repo):  # pylint: disable=invalid-name,missing-param-doc,missing-raises-doc,missing-return-doc
+    # pylint: disable=missing-return-type-doc,missing-type-doc
     """Update a repository. Return False if missing; return True if successful; raise an exception if updating fails."""
     assert os.path.isdir(repo)
-    repoType = typeOfRepo(repo)
+    repo_type = typeOfRepo(repo)
 
-    if repoType == 'hg':
+    if repo_type == 'hg':
         sps.timeSubprocess(['hg', 'pull', '-u'],
                            ignoreStderr=True, combineStderr=True, cwd=repo, vb=True)
         sps.timeSubprocess(['hg', 'log', '-r', 'default'], cwd=repo, vb=True)
-    elif repoType == 'git':
+    elif repo_type == 'git':
         # Ignore exit codes so the loop can continue retrying up to number of counts.
         gitenv = deepcopy(os.environ)
         if sps.isWin:
@@ -73,12 +75,12 @@ def updateRepo(repo):
         sps.timeSubprocess([GITBINARY, 'pull', '--rebase'], env=gitenv,
                            ignoreStderr=True, combineStderr=True, ignoreExitCode=True, cwd=repo, vb=True)
     else:
-        raise Exception('Unknown repository type: ' + repoType)
+        raise Exception('Unknown repository type: ' + repo_type)
 
     return True
 
 
-def updateRepos():
+def updateRepos():  # pylint: disable=invalid-name
     """Update Mercurial and Git repositories located in ~ and ~/trees ."""
     trees = [
         os.path.normpath(os.path.join(REPO_PARENT_PATH)),
@@ -86,19 +88,19 @@ def updateRepos():
     ]
     for tree in trees:
         for name in sorted(os.listdir(tree)):
-            namePath = os.path.join(tree, name)
-            if os.path.isdir(namePath) and (name in REPOS or name.startswith("funfuzz")):
+            name_path = os.path.join(tree, name)
+            if os.path.isdir(name_path) and (name in REPOS or name.startswith("funfuzz")):
                 print("Updating %s ..." % name)
-                updateRepo(namePath)
+                updateRepo(name_path)
 
 
-def main():
+def main():  # pylint: disable=missing-docstring
     logger.info(time.asctime())
     try:
         updateRepos()
-    except OSError as e:
+    except OSError as ex:
         print("WARNING: OSError hit:")
-        print(e)
+        print(ex)
     logger.info(time.asctime())
 
 
