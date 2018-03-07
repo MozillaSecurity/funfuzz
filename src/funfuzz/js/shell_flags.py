6b293eb22d10c07fd5ad1746d585ce2b335f6c31
Gary Kwong <nth10sd@gmail.com>
2018-03-07 11:20:07 -0800

    Refactor the random_flag_set function, split off some into other functions and add tests.

--- src/funfuzz/js/shell_flags.py
+++ src/funfuzz/js/shell_flags.py
@@ -50,166 +50,235 @@ def chance(i, always=False):
     return (random.random() < i) if not always else True
 
 
-def random_flag_set(shell_path):  # pylint: disable=too-complex,too-many-branches,too-many-statements
-    """Returns a random list of CLI flags appropriate for the given shell.
-
-    Only works for spidermonkey js shell. Does not work for xpcshell.
+def add_random_arch_flags(shell_path, input_list, always=False):
+    """Returns a list with probably additional architecture-related flags added.
 
     Args:
         shell_path (str): Path to the required shell.
+        input_list (list): List of flags to eventually be tested against a shell.
+        always (bool): Causes the chance function to always return True. For testing purposes.
+
+    Returns:
+        list: List of flags to be tested, with probable architecture-related flags added.
+    """
+    if inspect_shell.queryBuildConfiguration(shell_path, "arm-simulator") and chance(.7, always):
+        # m-c rev 165993:c450eb3abde4, see bug 965247
+        input_list.append("--arm-sim-icache-checks")
+
+    # m-c rev 222786:bcacb5692ad9 is the earliest known working revision, so stop testing prior existence of flag
+
+    if chance(.2, always):  # m-c rev 154600:526ba3ace37a, see bug 935791
+        input_list.append("--no-sse" + "3" if chance(.5, always) else "4")
+
+    return input_list
+
+
+def add_random_ion_flags(shell_path, input_list, always=False):  # pylint: disable=too-complex,too-many-branches
+    """Returns a list with probably additional IonMonkey flags added.
+
+    The non-default options have a higher chance of being set, e.g. chance(.9, always)
+
+    Args:
+        shell_path (str): Path to the required shell.
+        input_list (list): List of flags to eventually be tested against a shell.
+        always (bool): Causes the chance function to always return True. For testing purposes.
+
+    Returns:
+        list: List of flags to be tested, with probable IonMonkey flags added.
+    """
+    if shell_supports_flag(shell_path, "--ion-aa=flow-sensitive") and chance(.2, always):
+        # m-c rev 295435:c0c1d923c292, see bug 1255008
+        input_list.append("--ion-aa=flow-" + ("" if chance(.9, always) else "in") + "sensitive")
+    if shell_supports_flag(shell_path, "--ion-pgo=on") and chance(.2, always):
+        # m-c rev 272274:b0a0ff5fa705, see bug 1209515
+        input_list.append("--ion-pgo=" + "on" if chance(.1, always) else "off")
+    if shell_supports_flag(shell_path, "--ion-sincos=on") and chance(.2, always):
+        # m-c rev 262544:3dec2b935295, see bug 984018
+        input_list.append("--ion-sincos=" + "on" if chance(.5, always) else "off")
+    if shell_supports_flag(shell_path, "--ion-instruction-reordering=on") and chance(.2, always):
+        # m-c rev 259672:59d2f2e62420, see bug 1195545
+        input_list.append("--ion-instruction-reordering=" + "on" if chance(.9, always) else "off")
+    if shell_supports_flag(shell_path, "--ion-shared-stubs=on") and chance(.2, always):
+        # m-c rev 257573:3655d19ce241, see bug 1168756
+        input_list.append("--ion-shared-stubs=" + "on" if chance(.1, always) else "off")
+    if shell_supports_flag(shell_path, "--ion-regalloc=testbed") and chance(.2, always):
+        # m-c rev 248962:47e92bae09fd, see bug 1170840
+        input_list.append("--ion-regalloc=testbed")
+    if shell_supports_flag(shell_path, "--non-writable-jitcode") and chance(.2, always):
+        # m-c rev 248578:b46d6692fe50, see bug 977805
+        input_list.append("--non-writable-jitcode")
+    if (shell_supports_flag(shell_path, "--execute=setJitCompilerOption(\"ion.forceinlineCaches\",1)") and
+            chance(.2, always)):
+        # m-c rev 247709:ea9608e33abe, see bug 923717
+        input_list.append("--execute=setJitCompilerOption(\"ion.forceinlineCaches\",1)")
+    if shell_supports_flag(shell_path, "--ion-extra-checks") and chance(.2, always):
+        # m-c rev 234228:cdf93416b39a, see bug 1139152
+        input_list.append("--ion-extra-checks")
+
+    # m-c rev 222786:bcacb5692ad9 is the earliest known working revision, so stop testing prior existence of flag
+
+    # --ion-sink=on is still not ready to be fuzzed
+    # if chance(.2, always):  # m-c rev 217242:9188c8b7962b, see bug 1093674
+    #     input_list.append("--ion-sink=" + "on" if chance(.1, always) else "off")
+    if chance(.2, always):  # m-c rev 198804:aa33f4725177, see bug 1039458
+        input_list.append("--ion-loop-unrolling=" + "on" if chance(.9, always) else "off")
+    if chance(.2, always):  # m-c rev 142933:f08e4a699011, see bug 894813
+        input_list.append("--ion-check-range-analysis")
+    # The stupid allocator isn't used by default and devs prefer not to have to fix fuzzbugs
+    # if chance(.2, always):  # m-c rev 114120:7e97c5392d81, see bug 812945
+        # input_list.append("--ion-regalloc=stupid")
+    if chance(.2, always):  # m-c rev 106493:6688ede89a36, see bug 699883
+        input_list.append("--ion-range-analysis=" + "on" if chance(.1, always) else "off")
+    if chance(.2, always):  # m-c rev 106491:6c870a497ea4, see bug 699883
+        input_list.append("--ion-edgecase-analysis=" + "on" if chance(.1, always) else "off")
+    if chance(.2, always):  # m-c rev 106247:feac7727629c, see bug 755010
+        input_list.append("--ion-limit-script-size=" + "on" if chance(.1, always) else "off")
+    if chance(.2, always):  # m-c rev 105351:9fb668f0baca, see bug 700108
+        input_list.append("--ion-osr=" + "on" if chance(.1, always) else "off")
+    if chance(.2, always):  # m-c rev 105338:01ebfabf29e2, see bug 687901
+        input_list.append("--ion-inlining=" + "on" if chance(.1, always) else "off")
+    if chance(.7, always):  # m-c rev 105173:4ceb3e9961e4, see bug 683039
+        input_list.append("--ion-eager")
+    if chance(.2, always):  # m-c rev 104923:8db8eef79b8c, see bug 670816
+        input_list.append("--ion-gvn=" + "on" if chance(.1, always) else "off")
+    if chance(.2, always):  # m-c rev 104923:8db8eef79b8c, see bug 670816
+        input_list.append("--ion-licm=" + "on" if chance(.1, always) else "off")
+
+    return input_list
+
+
+def add_random_wasm_flags(shell_path, input_list, always=False):
+    """Returns a list with probably additional wasm flags added.
+
+    Args:
+        shell_path (str): Path to the required shell.
+        input_list (list): List of flags to eventually be tested against a shell.
+        always (bool): Causes the chance function to always return True. For testing purposes.
+
+    Returns:
+        list: List of flags to be tested, with probable wasm flags added.
+    """
+    if shell_supports_flag(shell_path, "--test-wasm-await-tier2") and chance(.8, always):
+        # m-c rev 387188:b1dc87a94262, see bug 1388785
+        input_list.append("--test-wasm-await-tier2")
+
+    # m-c rev 222786:bcacb5692ad9 is the earliest known working revision, so stop testing prior existence of flag
+
+    return input_list
+
+
+def random_flag_set(shell_path, always=False):  # pylint: disable=too-complex,too-many-branches,too-many-statements
+    """Returns a random list of CLI flags appropriate for the given shell.
+
+    Args:
+        shell_path (str): Path to the required shell.
+        always (bool): Causes the chance function to always return True. For testing purposes.
 
     Returns:
         list: List of flags to be tested.
     """
     args = []
 
-    ion = shell_supports_flag(shell_path, "--ion") and chance(.8)
+    if shell_supports_flag(shell_path, "--fuzzing-safe"):  # This is always enabled if supported
+        # m-c rev 135892:0a9314155404, see bug 885361
+        args.append("--fuzzing-safe")
 
-    if shell_supports_flag(shell_path, "--fuzzing-safe"):
-        args.append("--fuzzing-safe")  # --fuzzing-safe landed in bug 885361
+    # Add other groups of flags randomly
+    if shell_supports_flag(shell_path, "--no-wasm"):
+        # m-c rev 321230:e9b561d60697, see bug 1313180
+        args = add_random_wasm_flags(shell_path, args, always)
 
-    # Landed in m-c changeset 399868:a98f615965d7, see bug 1430053
-    if shell_supports_flag(shell_path, "--spectre-mitigations=on") and chance(.3):
-        args.append("--spectre-mitigations=on" if chance(.9) else "--spectre-mitigations=off")
+    if shell_supports_flag(shell_path, "--no-sse3"):
+        # m-c rev 154600:526ba3ace37a, see bug 935791
+        args = add_random_arch_flags(shell_path, args, always)
 
-    # Landed in m-c changeset c0c1d923c292, see bug 1255008
-    if shell_supports_flag(shell_path, "--ion-aa=flow-sensitive"):
-        if chance(.4):
-            args.append("--ion-aa=flow-sensitive")
-        elif shell_supports_flag(shell_path, "--ion-aa=flow-insensitive") and chance(.4):
-            args.append("--ion-aa=flow-insensitive")
+    if shell_supports_flag(shell_path, "--ion") and chance(.7, always):
+        # m-c rev 104923:8db8eef79b8c, see bug 670816
+        args = add_random_ion_flags(shell_path, args, always)
+    elif shell_supports_flag(shell_path, "--no-ion"):
+        # m-c rev 106120:300ac3d58291, see bug 724751
+        args.append("--no-ion")
 
-    # Note for future: --wasm-check-bce is only useful for x86 and ARM32
+    # Other flags
+    if shell_supports_flag(shell_path, "--spectre-mitigations=on") and chance(.2, always):
+        # m-c rev 399868:a98f615965d7, see bug 1430053
+        args.append("--spectre-mitigations=" + "on" if chance(.9, always) else "off")
 
-    if shell_supports_flag(shell_path, "--wasm-always-baseline") and chance(.5):
-        args.append("--wasm-always-baseline")  # --wasm-always-baseline landed in bug 1232205
+    # m-c rev 380023:1b55231e6628, see bug 1206770
+    if shell_supports_flag(shell_path, "--cpu-count=1"):
+        if chance(.7, always):
+            # Focus on the reproducible cases
+            # m-c rev 188900:9ab3b097f304, see bug 1020364
+            args.append("--ion-offthread-compile=" + "on" if chance(.1, always) else "off")
+        elif (chance(.5, always) and multiprocessing.cpu_count() > 1 and
+              shell_supports_flag(shell_path, "--cpu-count=1")):
+            # Adjusts default number of threads for offthread compilation (turned on by default)
+            args.append("--cpu-count=%s" % random.randint(2, (multiprocessing.cpu_count() * 2)))
 
-    if shell_supports_flag(shell_path, "--ion-pgo=on") and chance(.2):
-        args.append("--ion-pgo=on")  # --ion-pgo=on landed in bug 1209515
+    if shell_supports_flag(shell_path, "--no-unboxed-objects") and chance(.2, always):
+        # m-c rev 244297:322487136b28, see bug 1162199
+        args.append("--no-unboxed-objects")
 
-    if shell_supports_flag(shell_path, "--ion-sincos=on") and chance(.5):
-        sincos_switch = "on" if chance(0.5) else "off"
-        args.append("--ion-sincos=" + sincos_switch)  # --ion-sincos=[on|off] landed in bug 984018
+    if shell_supports_flag(shell_path, "--no-cgc") and chance(.2, always):
+        # m-c rev 226540:ade5e0300605, see bug 1126769
+        args.append("--no-cgc")
 
-    if shell_supports_flag(shell_path, "--ion-instruction-reordering=on") and chance(.2):
-        args.append("--ion-instruction-reordering=on")  # --ion-instruction-reordering=on landed in bug 1195545
+    if shell_supports_flag(shell_path, "--gc-zeal=0;0,1") and chance(.9, always):
+        allocations_number = 999 if chance(0, always) else random.randint(0, 500)  # 999 is for tests
 
-    if shell_supports_flag(shell_path, "--ion-shared-stubs=on") and chance(.2):
-        args.append("--ion-shared-stubs=on")  # --ion-shared-stubs=on landed in bug 1168756
-
-    if shell_supports_flag(shell_path, "--non-writable-jitcode") and chance(.3):
-        args.append("--non-writable-jitcode")  # --non-writable-jitcode landed in bug 977805
-
-    if shell_supports_flag(shell_path, "--execute=setJitCompilerOption(\"ion.forceinlineCaches\",1)") and chance(.1):
-        args.append("--execute=setJitCompilerOption(\"ion.forceinlineCaches\",1)")
-
-    if shell_supports_flag(shell_path, "--no-cgc") and chance(.1):
-        args.append("--no-cgc")  # --no-cgc landed in bug 1126769
-
-    if shell_supports_flag(shell_path, "--no-ggc") and chance(.1):
-        args.append("--no-ggc")  # --no-ggc landed in bug 706885
-
-    if shell_supports_flag(shell_path, "--no-incremental-gc") and chance(.1):
-        args.append("--no-incremental-gc")  # --no-incremental-gc landed in bug 958492
-
-    if shell_supports_flag(shell_path, "--no-unboxed-objects") and chance(.2):
-        args.append("--no-unboxed-objects")  # --no-unboxed-objects landed in bug 1162199
-
-    # if shell_supports_flag(shell_path, "--ion-sink=on") and chance(.2):
-    #    args.append("--ion-sink=on")  # --ion-sink=on landed in bug 1093674
-
-    if shell_supports_flag(shell_path, "--gc-zeal=0") and chance(.9):
         # Focus testing on CheckGrayMarking (18), see:
         #     https://hg.mozilla.org/mozilla-central/rev/bdbb5822afe1
-        gczeal_value = 18 if chance(0.5) else random.randint(0, 18)
+        highest_gczeal = 18
+        gczeal_value = highest_gczeal if chance(.2, always) else random.randint(0, highest_gczeal)
         # Repurpose gczeal modes 3, 5 and 6 since they do not exist.
         if gczeal_value == 3:
             gczeal_value = 0
         if gczeal_value == 5 or gczeal_value == 6:
             gczeal_value = 2
-        args.append("--gc-zeal=" + str(gczeal_value))  # --gc-zeal= landed in bug 1101602
 
-    if shell_supports_flag(shell_path, "--enable-small-chunk-size") and chance(.1):
-        args.append("--enable-small-chunk-size")  # --enable-small-chunk-size landed in bug 941804
+        gczeal_two = highest_gczeal if chance(.2, always) else random.randint(0, highest_gczeal)
+        # Repurpose gczeal modes 3, 5 and 6 since they do not exist.
+        if gczeal_two == 3:
+            gczeal_two = 0
+        if gczeal_two == 5 or gczeal_two == 6:
+            gczeal_two = 2
 
-    if shell_supports_flag(shell_path, "--ion-loop-unrolling=on") and chance(.2):
-        args.append("--ion-loop-unrolling=on")  # --ion-loop-unrolling=on landed in bug 1039458
+        # m-c rev 216625:03c6a758c9e8, see bug 1101602
+        args.append("--gc-zeal=%s;%s,%s" % (gczeal_value, gczeal_two, allocations_number))
 
-    if shell_supports_flag(shell_path, "--no-threads") and chance(.5):
-        args.append("--no-threads")  # --no-threads landed in bug 1031529
+    if shell_supports_flag(shell_path, "--no-incremental-gc") and chance(.2, always):
+        # m-c rev 211115:35025fd9e99b, see bug 958492
+        args.append("--no-incremental-gc")
 
-    if shell_supports_flag(shell_path, "--disable-ion") and chance(.05):
-        args.append("--disable-ion")  # --disable-ion landed in bug 789319
+    if shell_supports_flag(shell_path, "--no-threads") and chance(.5, always):
+        # m-c rev 195996:35038c3324ee, see bug 1031529
+        args.append("--no-threads")
 
-    if shell_supports_flag(shell_path, "--no-native-regexp") and chance(.1):
-        args.append("--no-native-regexp")  # See bug 976446
+    if shell_supports_flag(shell_path, "--no-native-regexp") and chance(.2, always):
+        # m-c rev 183413:43acd23f5a98, see bug 976446
+        args.append("--no-native-regexp")
 
-    if inspect_shell.queryBuildConfiguration(shell_path, "arm-simulator") and chance(.4):
-        args.append("--arm-sim-icache-checks")
-
-    if (shell_supports_flag(shell_path, "--no-sse3") and shell_supports_flag(shell_path, "--no-sse4")) and chance(.2):
-        # --no-sse3 and --no-sse4 landed in m-c rev 526ba3ace37a.
-        if chance(.5):
-            args.append("--no-sse3")
-        else:
-            args.append("--no-sse4")
-
-    if shell_supports_flag(shell_path, "--no-asmjs") and chance(.5):
-        args.append("--no-asmjs")
+    if shell_supports_flag(shell_path, "--no-ggc") and chance(.2, always):
+        # m-c rev 129273:3297733a2661, see bug 706885
+        args.append("--no-ggc")
 
     # --baseline-eager landed after --no-baseline on the IonMonkey branch prior to landing on m-c.
     if shell_supports_flag(shell_path, "--baseline-eager"):
-        if chance(.3):
+        if chance(.2, always):
+            # m-c rev 127126:1c0489e5a302, see bug 818231
             args.append("--no-baseline")
         # elif is important, as we want to call --baseline-eager only if --no-baseline is not set.
-        elif chance(.6):
+        elif chance(.5, always):
+            # m-c rev 127353:be125cabea26, see bug 843596
             args.append("--baseline-eager")
 
-    # Landed in m-c changeset 380023:1b55231e6628, see bug 1206770
-    if shell_supports_flag(shell_path, "--cpu_count=1"):
-        if chance(.7):
-            # Focus on the reproducible cases
-            args.append("--ion-offthread-compile=off")
-        elif chance(.5) and multiprocessing.cpu_count() > 1 and shell_supports_flag(shell_path, "--cpu-count=1"):
-            # Adjusts default number of threads for parallel compilation (turned on by default)
-            total_threads = random.randint(2, (multiprocessing.cpu_count() * 2))
-            args.append("--cpu-count=" + str(total_threads))
+    if shell_supports_flag(shell_path, "--no-asmjs") and chance(.7, always):
+        # m-c rev 124920:b3d85b68449d, see bug 840282
+        args.append("--no-asmjs")
 
-    if ion:
-        if chance(.6):
-            args.append("--ion-eager")
-        if chance(.2):
-            args.append("--ion-gvn=off")
-        if chance(.2):
-            args.append("--ion-licm=off")
-        if shell_supports_flag(shell_path, "--ion-edgecase-analysis=off") and chance(.2):
-            args.append("--ion-edgecase-analysis=off")
-        if chance(.2):
-            args.append("--ion-range-analysis=off")
-        if chance(.2):
-            args.append("--ion-inlining=off")
-        if chance(.2):
-            args.append("--ion-osr=off")
-        if chance(.2):
-            args.append("--ion-limit-script-size=off")
-        # Backtracking (on by default as of 2015-04-15) and stupid landed in m-c changeset dc4887f61d2e
-        # The stupid allocator isn't used by default and devs prefer not to have to fix fuzzbugs
-        # if shell_supports_flag(shell_path, "--ion-regalloc=stupid") and chance(.2):
-            # args.append("--ion-regalloc=stupid")
-        if shell_supports_flag(shell_path, "--ion-regalloc=testbed") and chance(.2):
-            args.append("--ion-regalloc=testbed")
-        if shell_supports_flag(shell_path, "--ion-check-range-analysis") and chance(.3):
-            args.append("--ion-check-range-analysis")
-        if shell_supports_flag(shell_path, "--ion-extra-checks") and chance(.3):
-            args.append("--ion-extra-checks")
-    else:
-        args.append("--no-ion")
-
-    # if chance(.05):
-    #    args.append("--execute=verifyprebarriers()")
-
-    if chance(.05):
-        args.append("-D")  # aka --dump-bytecode
+    if shell_supports_flag(shell_path, "--dump-bytecode") and chance(.05, always):
+        # m-c rev 73054:b1923b866d6a, see bug 668095
+        args.append("--dump-bytecode")
 
     return args
 
--- tests/js/test_shell_flags.py
+++ tests/js/test_shell_flags.py
@@ -44,6 +44,46 @@ def get_current_shell_path():
                         "js-dbg-optDisabled-64-dm-vg-oombp-linux-" + hg_hash_of_default)
 
 
+def test_add_random_arch_flags():
+    """Test that we are able to obtain add shell runtime flags related to architecture."""
+    all_flags = funfuzz.js.shell_flags.add_random_arch_flags(get_current_shell_path(), [], always=True)
+    assert "--no-sse3" in all_flags
+    if funfuzz.js.inspect_shell.queryBuildConfiguration(get_current_shell_path(), "arm-simulator"):
+        assert "--arm-sim-icache-checks" in all_flags
+
+
+def test_add_random_ion_flags():
+    """Test that we are able to obtain add shell runtime flags related to IonMonkey."""
+    all_flags = funfuzz.js.shell_flags.add_random_ion_flags(get_current_shell_path(), [], always=True)
+    assert "--ion-aa=flow-sensitive" in all_flags
+    assert "--ion-pgo=on" in all_flags
+    assert "--ion-sincos=on" in all_flags
+    assert "--ion-instruction-reordering=on" in all_flags
+    assert "--ion-shared-stubs=on" in all_flags
+    assert "--ion-regalloc=testbed" in all_flags
+    assert "--non-writable-jitcode" in all_flags
+    assert '--execute=setJitCompilerOption("ion.forceinlineCaches",1)' in all_flags
+    assert "--ion-extra-checks" in all_flags
+    # assert "--ion-sink=on" in all_flags
+    assert "--ion-loop-unrolling=on" in all_flags
+    assert "--ion-check-range-analysis" in all_flags
+    # assert "--ion-regalloc=stupid" in all_flags
+    assert "--ion-range-analysis=on" in all_flags
+    assert "--ion-edgecase-analysis=on" in all_flags
+    assert "--ion-limit-script-size=on" in all_flags
+    assert "--ion-osr=on" in all_flags
+    assert "--ion-inlining=on" in all_flags
+    assert "--ion-eager" in all_flags
+    assert "--ion-gvn=on" in all_flags
+    assert "--ion-licm=on" in all_flags
+
+
+def test_add_random_wasm_flags():
+    """Test that we are able to obtain add shell runtime flags related to WebAssembly (wasm)."""
+    all_flags = funfuzz.js.shell_flags.add_random_wasm_flags(get_current_shell_path(), [], always=True)
+    assert "--test-wasm-await-tier2" in all_flags
+
+
 def test_basic_flag_sets():
     """Test that we are able to obtain a basic set of shell runtime flags for fuzzing."""
     important_flag_set = ["--fuzzing-safe", "--no-threads", "--ion-eager"]  # Important flag set combination
@@ -54,6 +94,28 @@ def test_chance():
     """Test that the chance function works as intended."""
     assert funfuzz.js.shell_flags.chance(0.6, always=True)
     assert funfuzz.js.shell_flags.chance(0.1, always=True)
+    assert not funfuzz.js.shell_flags.chance(0, always=False)
+
+
+def test_random_flag_set():
+    """Test runtime flags related to SpiderMonkey."""
+    assert "--fuzzing-safe" in funfuzz.js.shell_flags.random_flag_set(get_current_shell_path(), always=False)
+
+    all_flags = funfuzz.js.shell_flags.random_flag_set(get_current_shell_path(), always=True)
+    assert "--fuzzing-safe" in all_flags
+    assert "--spectre-mitigations=on" in all_flags
+    assert "--ion-offthread-compile=on" in all_flags
+    assert "--no-unboxed-objects" in all_flags
+    assert "--no-cgc" in all_flags
+    highest_gczeal = 18
+    assert "--gc-zeal=%s;%s,%s" % (highest_gczeal, highest_gczeal, 999) in all_flags
+    assert "--no-incremental-gc" in all_flags
+    assert "--no-threads" in all_flags
+    assert "--no-native-regexp" in all_flags
+    assert "--no-ggc" in all_flags
+    assert "--no-baseline" in all_flags
+    assert "--no-asmjs" in all_flags
+    assert "--dump-bytecode" in all_flags
 
 
 def test_shell_supports_flag():
