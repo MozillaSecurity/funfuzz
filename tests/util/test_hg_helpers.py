dc693a781089b4f4bcd79cc9f1c10f58fc69f184
Gary Kwong <nth10sd@gmail.com>
2018-03-21 10:49:27 -0700

    Add docstrings for test_compile_shell.py and test_hg_helpers.py

--- tests/util/test_hg_helpers.py
+++ tests/util/test_hg_helpers.py
@@ -1,9 +1,11 @@
 # coding=utf-8
-# pylint: disable=invalid-name,missing-docstring
+# pylint: disable=invalid-name
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+"""Test the hg_helpers.py file."""
 
 from __future__ import absolute_import, unicode_literals
 
@@ -19,16 +21,16 @@ logging.getLogger("flake8").setLevel(logging.WARNING)
 
 
 class TestCase(unittest.TestCase):
-
+    """"TestCase class for general functions, e.g. backport ones."""
     if sys.version_info.major == 2:
-
         def assertRaisesRegex(self, *args, **kwds):  # pylint: disable=arguments-differ
-            # pylint: disable=missing-return-doc,missing-return-type-doc
+            # pylint: disable=missing-param-doc,missing-return-doc,missing-return-type-doc
+            """Adds support for raising exceptions with messages containing desired regex."""
             return self.assertRaisesRegexp(*args, **kwds)  # pylint: disable=deprecated-method
 
 
 class HgHelpersTests(TestCase):
-
+    """"TestCase class for functions in hg_helpers.py"""
     def test_get_cset_hash_from_bisect_msg(self):
         """Test that we are able to extract the changeset hash from bisection output."""
         self.assertEqual(funfuzz.util.hg_helpers.get_cset_hash_from_bisect_msg("x 12345:abababababab"), "abababababab")
