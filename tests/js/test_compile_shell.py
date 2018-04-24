c2cce68458cc4d43842215233ef4efc9395a874f
Gary Kwong <nth10sd@gmail.com>
2018-04-24 01:03:07 -0700

    Switch test_compile_shell to use environment variables especially for Travis instead.

--- tests/js/test_compile_shell.py
+++ tests/js/test_compile_shell.py
@@ -11,10 +11,16 @@ from __future__ import absolute_import, unicode_literals  # isort:skip
 
 import logging
 import os
+import sys
 
 import pytest
 
 import funfuzz
+
+if sys.version_info.major == 2:
+    from functools32 import lru_cache  # pylint: disable=import-error
+else:
+    from functools import lru_cache  # pylint: disable=no-name-in-module
 
 funfuzz_log = logging.getLogger("funfuzz_test")
 logging.basicConfig(level=logging.DEBUG)
@@ -28,42 +34,36 @@ slow_test = pytest.mark.xfail(is_ci_no_slow,
 
 
 @slow_test
-def test_compile_shell_A_dbg():
-    """Test compilation of a debug shell with determinism, valgrind and OOM breakpoint support."""
+@lru_cache(maxsize=None)
+def test_shell_compile():
+    """Test compilation of shells depending on the specified environment variable.
+
+    Returns:
+        str: Path to the compiled shell.
+    """
     assert os.path.isdir(os.path.join(os.path.expanduser("~"), "trees", "mozilla-central"))
-    # Remember to update the expected binary filename
-    build_opts = ("--enable-debug --disable-optimize --enable-more-deterministic "
-                  "--build-with-valgrind --enable-oom-breakpoint")
     # Change the repository location by uncommenting this line and specifying the right one
     # "-R ~/trees/mozilla-central/")
-    build_opts_processed = funfuzz.js.build_options.parseShellOptions(build_opts)
-    hg_hash_of_default = funfuzz.util.hg_helpers.getRepoHashAndId(build_opts_processed.repoDir)[0]
 
-    fz = funfuzz.js.compile_shell.CompiledShell(build_opts_processed, hg_hash_of_default)
+    default_parameters_debug = ("--enable-debug --disable-optimize --enable-more-deterministic "
+                                "--build-with-valgrind --enable-oom-breakpoint")
+    # Remember to update the corresponding BUILD build parameters in .travis.yml as well
+    build_opts = os.environ.get("BUILD", default_parameters_debug)
 
-    result = fz.run(["-b", build_opts])
-    assert result == 0
-    file_name = "js-dbg-optDisabled-64-dm-vg-oombp-linux-" + hg_hash_of_default
-    assert os.path.isfile(os.path.join(
-        os.path.expanduser("~"), "shell-cache", file_name, file_name))
+    opts_parsed = funfuzz.js.build_options.parseShellOptions(build_opts)
+    hg_hash_of_default = funfuzz.util.hg_helpers.getRepoHashAndId(opts_parsed.repoDir)[0]
+    # Ensure exit code is 0
+    assert not funfuzz.js.compile_shell.CompiledShell(opts_parsed, hg_hash_of_default).run(["-b", build_opts])
 
+    if default_parameters_debug in build_opts:
+        # Test compilation of a debug shell with determinism, valgrind and OOM breakpoint support.
+        file_name = "js-dbg-optDisabled-64-dm-vg-oombp-linux-" + hg_hash_of_default
+    elif "--disable-debug --disable-profiling --without-intl-api" in build_opts:
+        # Test compilation of an opt shell with both profiling and Intl support disabled.
+        # This set of builds should also have the following: 32-bit with ARM, with asan, and with clang
+        file_name = "js-64-profDisabled-intlDisabled-linux-" + hg_hash_of_default
 
-@slow_test
-def test_compile_shell_B_opt():
-    """Test compilation of an opt shell with both profiling and Intl support disabled."""
-    assert os.path.isdir(os.path.join(os.path.expanduser("~"), "trees", "mozilla-central"))
-    # Remember to update the expected binary filename
-    build_opts = ("--disable-debug --disable-profiling --without-intl-api")
-    # Change the repository location by uncommenting this line and specifying the right one
-    # "-R ~/trees/mozilla-central/")
-    build_opts_processed = funfuzz.js.build_options.parseShellOptions(build_opts)
-    hg_hash_of_default = funfuzz.util.hg_helpers.getRepoHashAndId(build_opts_processed.repoDir)[0]
+    compiled_bin = os.path.join(os.path.expanduser("~"), "shell-cache", file_name, file_name)
+    assert os.path.isfile(compiled_bin)
 
-    fz = funfuzz.js.compile_shell.CompiledShell(build_opts_processed, hg_hash_of_default)
-
-    # This set of builds should also have the following: 32-bit with ARM, with asan, and with clang
-    result = fz.run(["-b", build_opts])
-    assert result == 0
-    file_name = "js-64-profDisabled-intlDisabled-linux-" + hg_hash_of_default
-    assert os.path.isfile(os.path.join(
-        os.path.expanduser("~"), "shell-cache", file_name, file_name))
+    return compiled_bin
--- tests/js/test_shell_flags.py
+++ tests/js/test_shell_flags.py
@@ -11,16 +11,12 @@ from __future__ import absolute_import, unicode_literals  # isort:skip
 
 import logging
 import os
-import sys
 
 import pytest
 
 import funfuzz
 
-if sys.version_info.major == 2:
-    from functools32 import lru_cache  # pylint: disable=import-error
-else:
-    from functools import lru_cache  # pylint: disable=no-name-in-module
+from .test_compile_shell import test_shell_compile
 
 funfuzz_log = logging.getLogger("funfuzz_test")
 logging.basicConfig(level=logging.DEBUG)
@@ -31,27 +27,6 @@ is_ci_no_slow = ("CI" in os.environ and os.environ["CI"] == "true" and
 slow_test = pytest.mark.xfail(is_ci_no_slow,
                               raises=AssertionError,
                               reason="NO_SLOW is true, so skipping this test on Travis CI.")
-
-
-@lru_cache(maxsize=None)
-def get_current_shell_path():
-    """Retrieve the path to the currently built shell.
-
-    Returns:
-        str: Path to the compiled shell.
-    """
-    assert os.path.isdir(os.path.join(os.path.expanduser("~"), "trees", "mozilla-central"))
-    # Remember to update the expected binary filename
-    build_opts = ("--enable-debug --disable-optimize --enable-more-deterministic "
-                  "--build-with-valgrind --enable-oom-breakpoint")
-    # Change the repository location by uncommenting this line and specifying the right one
-    # "-R ~/trees/mozilla-central/")
-    build_opts_processed = funfuzz.js.build_options.parseShellOptions(build_opts)
-    hg_hash_of_default = funfuzz.util.hg_helpers.getRepoHashAndId(build_opts_processed.repoDir)[0]
-
-    return os.path.join(os.path.expanduser("~"), "shell-cache",
-                        "js-dbg-optDisabled-64-dm-vg-oombp-linux-" + hg_hash_of_default,
-                        "js-dbg-optDisabled-64-dm-vg-oombp-linux-" + hg_hash_of_default)
 
 
 def mock_chance(i):
@@ -75,10 +50,10 @@ def test_add_random_arch_flags(monkeypatch):
     """
     monkeypatch.setattr(funfuzz.js.shell_flags, "chance", mock_chance)
 
-    all_flags = funfuzz.js.shell_flags.add_random_arch_flags(get_current_shell_path(), [])
+    all_flags = funfuzz.js.shell_flags.add_random_arch_flags(test_shell_compile(), [])
     assert "--enable-avx" in all_flags
     assert "--no-sse3" in all_flags
-    if funfuzz.js.inspect_shell.queryBuildConfiguration(get_current_shell_path(), "arm-simulator"):
+    if funfuzz.js.inspect_shell.queryBuildConfiguration(test_shell_compile(), "arm-simulator"):
         assert "--arm-sim-icache-checks" in all_flags
 
 
@@ -91,7 +66,7 @@ def test_add_random_ion_flags(monkeypatch):
     """
     monkeypatch.setattr(funfuzz.js.shell_flags, "chance", mock_chance)
 
-    all_flags = funfuzz.js.shell_flags.add_random_ion_flags(get_current_shell_path(), [])
+    all_flags = funfuzz.js.shell_flags.add_random_ion_flags(test_shell_compile(), [])
     assert "--cache-ir-stubs=on" in all_flags
     assert "--ion-pgo=on" in all_flags
     assert "--ion-sincos=on" in all_flags
@@ -125,7 +100,7 @@ def test_add_random_wasm_flags(monkeypatch):
     """
     monkeypatch.setattr(funfuzz.js.shell_flags, "chance", mock_chance)
 
-    all_flags = funfuzz.js.shell_flags.add_random_wasm_flags(get_current_shell_path(), [])
+    all_flags = funfuzz.js.shell_flags.add_random_wasm_flags(test_shell_compile(), [])
     assert "--wasm-gc" in all_flags
     assert "--no-wasm-baseline" in all_flags
     assert "--no-wasm-ion" in all_flags
@@ -136,7 +111,7 @@ def test_add_random_wasm_flags(monkeypatch):
 def test_basic_flag_sets():
     """Test that we are able to obtain a basic set of shell runtime flags for fuzzing."""
     important_flag_set = ["--fuzzing-safe", "--no-threads", "--ion-eager"]  # Important flag set combination
-    assert important_flag_set in funfuzz.js.shell_flags.basic_flag_sets(get_current_shell_path())
+    assert important_flag_set in funfuzz.js.shell_flags.basic_flag_sets(test_shell_compile())
 
 
 def test_chance(monkeypatch):
@@ -161,7 +136,7 @@ def test_random_flag_set(monkeypatch):
     """
     monkeypatch.setattr(funfuzz.js.shell_flags, "chance", mock_chance)
 
-    all_flags = funfuzz.js.shell_flags.random_flag_set(get_current_shell_path())
+    all_flags = funfuzz.js.shell_flags.random_flag_set(test_shell_compile())
     assert "--fuzzing-safe" in all_flags
     assert "--nursery-strings=on" in all_flags
     assert "--spectre-mitigations=on" in all_flags
@@ -182,4 +157,4 @@ def test_random_flag_set(monkeypatch):
 @slow_test
 def test_shell_supports_flag():
     """Test that the shell does support flags as intended."""
-    assert funfuzz.js.shell_flags.shell_supports_flag(get_current_shell_path(), "--fuzzing-safe")
+    assert funfuzz.js.shell_flags.shell_supports_flag(test_shell_compile(), "--fuzzing-safe")
