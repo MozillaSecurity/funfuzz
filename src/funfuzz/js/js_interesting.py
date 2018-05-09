5e115b4a4a7caf38a9e5dccd66d924488b280dc6
Gary Kwong <nth10sd@gmail.com>
2018-05-09 15:51:36 -0700

    Workaround issue involving UnicodeDecodeError and FuzzManager.

--- src/funfuzz/js/js_interesting.py
+++ src/funfuzz/js/js_interesting.py
@@ -159,10 +159,14 @@ class ShellResult(object):  # pylint: disable=missing-docstring,too-many-instanc
                 "Bus error" in str(crashInfo.rawStderr):
             lev = max(lev, JS_NEW_ASSERT_OR_CRASH)
 
-        match = options.collector.search(crashInfo)
-        if match[0] is not None:
-            create_collector.printMatchingSignature(match)
-            lev = JS_FINE
+        try:
+            match = options.collector.search(crashInfo)
+            if match[0] is not None:
+                create_collector.printMatchingSignature(match)
+                lev = JS_FINE
+        except UnicodeDecodeError:  # Sometimes FM throws due to unicode issues
+            print("Note: FuzzManager is throwing a UnicodeDecodeError, signature matching skipped")
+            match = False
 
         print("%s | %s" % (logPrefix, summaryString(issues, lev, runinfo.elapsedtime)))
 
