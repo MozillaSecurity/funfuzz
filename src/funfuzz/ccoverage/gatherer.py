059416a9225cd8171c3726a7e0a341457071b0b4
Gary Kwong <nth10sd@gmail.com>
2018-06-05 18:15:55 -0700

    Finish creating gatherer.py functionality.

--- src/funfuzz/ccoverage/gatherer.py
+++ src/funfuzz/ccoverage/gatherer.py
@@ -9,24 +9,20 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals  # isort:skip
 
+import configparser
+import io
 import logging
+import platform
 import sys
 
-# import os
-# import platform
-# import tarfile
-# import zipfile
-
-# import requests
+from ..bot import JS_SHELL_DEFAULT_TIMEOUT
+from ..js.loop import many_timed_runs
+from ..util import create_collector
 
 if sys.version_info.major == 2:
     import subprocess32 as subprocess  # pylint: disable=import-error
-#     import backports.tempfile as tempfile  # pylint: disable=import-error,no-name-in-module
-#     from pathlib2 import Path
 else:
     import subprocess
-#     from pathlib import Path  # pylint: disable=import-error
-#     import tempfile
 
 RUN_COV_LOG = logging.getLogger("funfuzz")
 
@@ -37,23 +33,32 @@ def gather_coverage(dirpath):
     Args:
         dirpath (Path): Directory in which build is to be downloaded in.
     """
-    RUN_COV_LOG.info(str(dirpath))
-    # 25 - 100 runs
-    # For testing run for shorter time. Use Docker.
-    # Run for 23.9 hours for things to shut down
-    # programmatically in userdata/Docker get this to shut down the EC2SpotManager pool
-    # same mini-timeout as regular bot.py
+    RUN_COV_LOG.info("Coverage build is being run in the following directory: %s", str(dirpath))
+    bin_name = "js" + (".exe" if platform.system() == "Windows" else "")
+    cov_build_bin_path = dirpath / "cov-build" / "dist" / "bin" / bin_name
+    assert cov_build_bin_path.is_file()  # pylint: disable=no-member
+    loop_args = ["--compare-jit", "--random-flags",
+                 str(JS_SHELL_DEFAULT_TIMEOUT), "KNOWNPATH", str(cov_build_bin_path), "--fuzzing-safe"]
 
-    # GCOV_PREFIX_STRIP=13
-    # GCOV_PREFIX=~/<some dir>/buildcovjs/
-    #   buildcovjs/dist/bin/js --fuzzing-safe --no-threads --ion-eager
-    subprocess.run(["ls"], check=True)
+    cov_timeout = 60
+    # cov_timeout = 85000  # 85,000 seconds is just under a day
+    RUN_COV_LOG.info("Fuzzing a coverage build for %s seconds...", str(cov_timeout))
+    many_timed_runs(cov_timeout, dirpath, loop_args, create_collector.make_collector(), True)
+    RUN_COV_LOG.info("Finished fuzzing the coverage build")
 
-    # ./grcov
-    # ~/<some dir>/buildcovjs/
-    # -t coveralls+
-    # --commit-sha d2d518b1f873
-    # --token NONE
-    #   -p /srv/jenkins/jobs/mozilla-central-clone/workspace/
-    # > cov.json
-    subprocess.run(["ls"], check=True)
+    fm_conf = configparser.SafeConfigParser()
+    fm_conf.read(str(dirpath / "cov-build" / "dist" / "bin" / "js.fuzzmanagerconf"))
+    RUN_COV_LOG.info("Generating grcov data...")
+    cov_output = subprocess.run([str(dirpath / "grcov-bin" / "grcov"), str(dirpath),
+                                 "-t", "coveralls+",
+                                 "--commit-sha", fm_conf.get("Main", "product_version"),
+                                 "--token", "NONE",
+                                 "-p", "/srv/jenkins/jobs/mozilla-central-clone/workspace/"],
+                                check=True,
+                                stdout=subprocess.PIPE).stdout.decode("utf-8", errors="replace")
+    RUN_COV_LOG.info("Finished generating grcov data")
+
+    RUN_COV_LOG.info("Writing grcov data to disk...")
+    with io.open(str(dirpath / "results_cov.json"), "w", encoding="utf-8", errors="replace") as f:
+        f.write(cov_output)
+    RUN_COV_LOG.info("Finished writing grcov data to disk")
