ed09f9da029471b9c191957d3ac708f691ab71d9
Gary Kwong <nth10sd@gmail.com>
2018-07-12 02:05:19 -0700

    Add python_requires line for Python 2.7 and Python 3.5 and up.

--- setup.py
+++ setup.py
@@ -62,4 +62,5 @@ if __name__ == "__main__":
               "whichcraft>=0.4.1",
           ],
           extras_require=EXTRAS,
+          python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
           zip_safe=False)
