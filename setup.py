4e41a1450eaf1f8825ca4680c5123781f3ed54dd
Gary Kwong <nth10sd@gmail.com>
2018-08-06 11:30:41 -0700

    Add docstring for setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 # coding=utf-8
-# pylint: disable=missing-docstring
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+"""setuptools install script"""
 
 from setuptools import setup
 
