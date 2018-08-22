10e41d24dd500afd97da7b540388f291f92d93e8
Gary Kwong <nth10sd@gmail.com>
2018-08-22 01:54:41 -0700

    Mac ASan builds submit properly to FuzzManager now.

--- src/funfuzz/js/build_options.py
+++ src/funfuzz/js/build_options.py
@@ -312,8 +312,6 @@ def areArgsValid(args):  # pylint: disable=invalid-name,missing-param-doc,missin
             return False, "32-bit ASan builds fail on 18.04 due to https://github.com/google/sanitizers/issues/954."
         if platform.system() == "Linux" and "Microsoft" in platform.release():
             return False, "Linux ASan builds cannot yet work in WSL though there may be workarounds."
-        if platform.system() == "Darwin":  # https://github.com/MozillaSecurity/funfuzz/issues/25
-            return False, "Mac ASan builds cannot yet submit to FuzzManager."
         if platform.system() == "Windows":
             return False, "Asan is not yet supported on Windows."
         if platform.system() == "Windows" and args.enable32:
