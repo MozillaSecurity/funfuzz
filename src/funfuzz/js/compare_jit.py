599a7bdbfaf2629eb62186cbc404dcb6077f5dcc
Gary Kwong <nth10sd@gmail.com>
2019-01-01 13:39:49 -0800

    Revert "Make sure autobisectjs log metadata has newlines for compare_jit reports."
    This reverts commit dd86fd82f2fa16ed3debca79562a693444a25baa.

--- src/funfuzz/js/compare_jit.py
+++ src/funfuzz/js/compare_jit.py
@@ -89,7 +89,7 @@ def compare_jit(jsEngine,  # pylint: disable=invalid-name,missing-param-doc,miss
 
         metadata = {}
         if autoBisectLog:
-            metadata = {"autoBisectLog": "\n".join(autoBisectLog)}
+            metadata = {"autoBisectLog": "".join(autoBisectLog)}
         options.collector.submit(cl[1], str(infilename), quality, metaData=metadata)
         return True
 
