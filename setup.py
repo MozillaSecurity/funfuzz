751107deeb3e0a544d9000dec8845b5686587e92
Gary Kwong <nth10sd@gmail.com>
2019-01-16 00:24:40 -0800

    Bump pytest to 4.1.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ EXTRAS = {
         "flake8-quotes==1.0.0",
         "isort==4.3.4",
         "pylint==2.2.2",
-        "pytest==3.10.1",
+        "pytest==4.1.1",
         "pytest-cov==2.6.1",
         "pytest-flake8==1.0.3",
         "pytest-pylint==0.14.0",
