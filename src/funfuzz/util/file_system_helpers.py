a6c89001ddb4431eaf3e13aa0ef99e92c9f33199
Gary Kwong [:gkw] <nth10sd@gmail.com>
2019-01-23 10:40:17 -0800

    Support macOS and Windows on Travis (#221), r=truber
    * Remove AppVeyor configuration as we are now using Windows on Travis.
    
    * Remove AppVeyor build status from README.md
    
    * Support macOS and Windows on Travis.
    
    * Rename and move rm_tree_incl_readonly to rm_tree_incl_readonly_files and add a test.
    
    * Fix erroneous if condition.
    
    * Also produce coverage reports for fast tests.
    
    * Fix a variable name that was totally wrong.
    
    * Use Path.chmod throughout funfuzz.
    
    * Ignore a 'too-complex' pylint McCabe message.

--- src/funfuzz/util/file_system_helpers.py
+++ src/funfuzz/util/file_system_helpers.py
@@ -7,6 +7,12 @@
 """Helper functions dealing with the files on the file system.
 """
 
+import errno
+from pathlib import Path
+import platform
+import shutil
+import stat
+
 
 def delete_logs(log_prefix):  # pylint: disable=too-complex
     """Whoever might call baseLevel should eventually call this function (unless a bug was found).
@@ -44,3 +50,33 @@ def delete_logs(log_prefix):  # pylint: disable=too-complex
     core_gzip = (log_prefix.parent / f"{log_prefix.stem}-core").with_suffix(".gz")
     if core_gzip.is_file():
         core_gzip.unlink()
+
+
+def handle_rm_readonly_files(_func, path, exc):
+    """Handle read-only files on Windows. Adapted from https://stackoverflow.com/a/21263493.
+
+    Args:
+        _func (function): Function which raised the exception
+        path (str): Path name passed to function
+        exc (exception): Exception information returned by sys.exc_info()
+
+    Raises:
+        OSError: Raised if the read-only files are unable to be handled
+    """
+    assert platform.system() == "Windows"
+    path = Path(path)
+    if exc[1].errno == errno.EACCES:
+        Path.chmod(path, stat.S_IWRITE)
+        assert path.is_file()
+        path.unlink()
+    else:
+        raise OSError("Unable to handle read-only files.")
+
+
+def rm_tree_incl_readonly_files(dir_tree):
+    """Remove a directory tree including all read-only files. Directories should not be read-only.
+
+    Args:
+        dir_tree (Path): Directory tree of files to be removed
+    """
+    shutil.rmtree(str(dir_tree), onerror=handle_rm_readonly_files if platform.system() == "Windows" else None)
