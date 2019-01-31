6d0133c4bf8bf276cc9069ab652135cc9f771421
Gary Kwong <nth10sd@gmail.com>
2019-01-30 18:04:04 -0800

    More cleanup script changes and ensure this shows a Travis error.

--- cleanup_run_linters_fast_pytests.sh
+++ cleanup_run_linters_fast_pytests.sh
@@ -3,20 +3,31 @@
 # Run this here in the funfuzz directory, e.g.:
 # ./cleanup_run_linters_fast_pytests.sh
 
+# Run shellcheck and bashate first
+find . -type d \( -name venv \) -prune -o -type f -name "*.sh" -print0 |
+    xargs -n 1 -0 shellcheck
+find . -type d \( -name venv \) -prune -o -type f -name "*.sh" -print0 |
+    xargs -n 1 -0 bashate
+
 # This script runs flake8, pytest "not slow" and pylint tests if they are installed in python3
-# Define the location of python3 using the $PY3_PATH variable, else `which python3` is used
-PY3="${PY3_PATH:-`which python3`}"
+# Define the location of python3 using the $PY3_PATH variable, else `command -v python3` is used
+PY3="${PY3_PATH:-$(command -v python3)}"
 FLAKE8_EC=0
 PYLINT_EC=0
 PYTEST_EC=0
 
 # Remove *.pyc, *.pyo and __pycache__ directories first
-$PY3 -c "for p in __import__('pathlib').Path('.').rglob('*.py[co]'): p.unlink()"
-$PY3 -c "for p in __import__('pathlib').Path('.').rglob('__pycache__'): p.rmdir()"
+CURR_PATHLIB_PATH="for p in __import__('pathlib').Path('.')"
+$PY3 -c "$CURR_PATHLIB_PATH.rglob('*.py[co]'): p.unlink()"
+$PY3 -c "$CURR_PATHLIB_PATH.rglob('__pycache__'): p.rmdir()"
 
 # Run flake8
 if $PY3 -m flake8 --version > /dev/null 2>&1; then
-    $PY3 -m flake8 . || { FLAKE8_EC=$?; printf '%s\n' "flake8 found errors, exiting early." >&2; exit "$FLAKE8_EC";};
+    $PY3 -m flake8 . || {
+        FLAKE8_EC=$?;
+        printf '%s\n' "flake8 found errors, exiting early." >&2;
+        exit "$FLAKE8_EC";
+    };
     echo "flake8 finished running.";
 else
     echo "flake8 module is not installed in $PY3"
@@ -24,7 +35,10 @@ fi
 
 # Run pytest "not slow" tests
 if $PY3 -m pytest --version > /dev/null 2>&1; then
-    $PY3 -m pytest -q -m "not slow" || { PYTEST_EC=$?; printf '%s\n' "pytest found \"not slow\" test errors." >&2;};
+    $PY3 -m pytest -q -m "not slow" || {
+        PYTEST_EC=$?;
+        printf '%s\n' "pytest found \"not slow\" test errors." >&2;
+    };
     echo "pytest finished running."
 else
     echo "pytest module is not installed in $PY3"
@@ -32,8 +46,10 @@ fi
 
 # Run pylint
 if $PY3 -m pylint --version > /dev/null 2>&1; then
-    for i in $( ls -F `pwd`| grep 'src/\|tests/' ); do
-        $PY3 -m pylint $i || { PYLINT_EC=$?; printf '%s\n' "pylint found errors." >&2;};
+    for i in $( ls -F "$(pwd)"| grep "src/\|tests/" ); do
+        $PY3 -m pylint "$i" || {
+            PYLINT_EC=$?; printf '%s\n' "pylint found errors." >&2;
+        };
     done
     echo "pylint finished running."
 else
