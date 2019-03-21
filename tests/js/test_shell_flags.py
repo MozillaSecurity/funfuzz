314c191d14c2707e55bc61a9b19848384b434388
Gary Kwong <nth10sd@gmail.com>
2019-03-21 16:31:03 -0700

    Fix shell_flags test.

--- tests/js/test_shell_flags.py
+++ tests/js/test_shell_flags.py
@@ -59,7 +59,6 @@ def test_add_random_ion_flags(monkeypatch):
     assert "--ion-extra-checks" in all_flags
     # assert "--ion-sink=on" in all_flags
     assert "--ion-warmup-threshold=0" in all_flags
-    assert "--ion-warmup-threshold=100" in all_flags
     assert "--ion-scalar-replacement=on" in all_flags
     assert "--ion-check-range-analysis" in all_flags
     # assert "--ion-regalloc=stupid" in all_flags
