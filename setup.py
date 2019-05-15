282de2de959773891c67b8b67db7f4df4f615a43
Gary Kwong <nth10sd@gmail.com>
2019-05-14 19:28:51 -0700

    Bump isort to 4.3.20

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ EXTRAS = {
         "flake8-commas==2.0.0",
         "flake8-isort==2.7.0",
         "flake8-quotes==2.0.1",
-        "isort==4.3.19",
+        "isort==4.3.20",
         "pylint==2.3.1",
         "pytest==4.5.0",
         "pytest-cov==2.7.1",
