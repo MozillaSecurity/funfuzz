d410d08e4e27efc642f80db7e62f2198692537a1
Gary Kwong <nth10sd@gmail.com>
2019-06-19 13:43:27 -0700

    We no longer need to monkeypatch dumpScopeChain to an empty function when running compare_jit as its output dumping mode has been disabled in more-deterministic builds, as per bug 1521338.

--- src/funfuzz/js/loop.py
+++ src/funfuzz/js/loop.py
@@ -371,7 +371,6 @@ def jitCompareLines(jsfunfuzzOutputFilename, marker):  # pylint: disable=invalid
         "clearMarkObservers = function() { };\n",
         "dumpHeap = function() { };\n",
         "dumpObject = function() { };\n",
-        "dumpScopeChain = function() { };\n",
         "dumpStringRepresentation = function() { };\n",
         "evalInCooperativeThread = function() { };\n",
         "evalInWorker = function() { };\n",
