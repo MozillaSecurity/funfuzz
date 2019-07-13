4a8e3f601e5ca4459eee7db4d264f09e95291992
Gary Kwong <nth10sd@gmail.com>
2019-07-12 23:43:38 -0700

    Flipped condition for disabling cranelift with ASan builds.

--- src/funfuzz/js/compile_shell.py
+++ src/funfuzz/js/compile_shell.py
@@ -465,13 +465,13 @@ def cfgBin(shell):  # pylint: disable=invalid-name,missing-param-doc,missing-rai
 
     if (
             (
+                shell.build_opts.enableAddressSanitizer
                 # Disable cranelift on ASan builds if repository revision is on/after
                 #   m-c rev 428135:6fcf54117a3b21164e6e769343416d2262991f6e, fx63
-                shell.build_opts.enableAddressSanitizer
-                # Uncomment when bug 1565768 is fixed, disable cranelift on all ASan builds throughout
-                # and not hg_helpers.existsAndIsAncestor(shell.get_repo_dir(),
-                #                                        shell.get_hg_hash(),
-                #                                        "parents(6fcf54117a3b21164e6e769343416d2262991f6e)")
+                # Remove when bug 1565768 is fixed, disable cranelift on all ASan builds throughout for now
+                and not hg_helpers.existsAndIsAncestor(shell.get_repo_dir(),
+                                                       shell.get_hg_hash(),
+                                                       "parents(6fcf54117a3b21164e6e769343416d2262991f6e)")
             ) or (
                 # Disable cranelift if:
                 #   repository revision is on/after m-c rev 438680:4d9500ca5761edd678a109b6b5a4ac3f4aa5edb0, fx64
