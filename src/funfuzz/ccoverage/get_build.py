ae1340c09b36900efad885c65bbe3abd2b0d39f8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 21:37:25 +0000

    Adding tarfile member sanitization to extractall()

--- src/funfuzz/ccoverage/get_build.py
+++ src/funfuzz/ccoverage/get_build.py
@@ -97,7 +97,29 @@ def get_grcov(dirpath, args):
         grcov_bin_folder = dirpath / "grcov-bin"
         grcov_bin_folder.mkdir(parents=True, exist_ok=True)  # Ensure this dir has been created for Python 3.5 reasons
         with tarfile.open(fileobj=io.BytesIO(grcov_request.content), mode="r:bz2") as f:
-            f.extractall(str(grcov_bin_folder.resolve()))
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, str(grcov_bin_folder.resolve()))
 
     RUN_COV_LOG.info("grcov tarball extracted to this folder: %s", grcov_bin_folder.resolve())
     grcov_bin = grcov_bin_folder / f'grcov{".exe" if platform.system() == "Windows" else ""}'
