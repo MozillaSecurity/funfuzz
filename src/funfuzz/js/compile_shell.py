68ac3c0232c5dc9a086bcd2539672ebb9f3368db
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 21:37:25 +0000

    Adding tarfile member sanitization to extractall()

--- src/funfuzz/js/compile_shell.py
+++ src/funfuzz/js/compile_shell.py
@@ -647,7 +647,26 @@ def obtainShell(shell, updateToRev=None, updateLatestTxt=False):  # pylint: disa
                                     str(shell.get_s3_tar_with_ext_full_path())):
             print("Extracting shell...")
             with tarfile.open(str(shell.get_s3_tar_with_ext_full_path()), "r") as f:
-                f.extractall(str(shell.get_shell_cache_dir()))
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, str(shell.get_shell_cache_dir()))
             # Delete tarball after downloading from S3
             shell.get_s3_tar_with_ext_full_path().unlink()
             if platform.system() == "Windows":
--- src/funfuzz/js/with_binaryen.py
+++ src/funfuzz/js/with_binaryen.py
@@ -64,7 +64,29 @@ def ensure_binaryen(url, version):
                 with requests.get(url, allow_redirects=True, stream=True) as binaryen_gzip_request:
                     try:
                         with tarfile.open(fileobj=io.BytesIO(binaryen_gzip_request.content), mode="r:gz") as f:
-                            f.extractall(str(shell_cache.resolve()))
+                            
+                            import os
+                            
+                            def is_within_directory(directory, target):
+                                
+                                abs_directory = os.path.abspath(directory)
+                                abs_target = os.path.abspath(target)
+                            
+                                prefix = os.path.commonprefix([abs_directory, abs_target])
+                                
+                                return prefix == abs_directory
+                            
+                            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                            
+                                for member in tar.getmembers():
+                                    member_path = os.path.join(path, member.name)
+                                    if not is_within_directory(path, member_path):
+                                        raise Exception("Attempted Path Traversal in Tar File")
+                            
+                                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                                
+                            
+                            safe_extract(f, str(shell_cache.resolve()))
                     except OSError:
                         print("binaryen tarfile threw an OSError")
                     break
